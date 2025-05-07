f2ee5f4932e302b61e5cb2a320e445a4423bab3b
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-07 22:54:05 +0700

    update

--- home.nix
+++ home.nix
@@ -113,5 +113,43 @@
   xsession.windowManager.bspwm.startupPrograms = [
     "${pkgs.ibus}/bin/ibus restart || ${pkgs.ibus}/bin/ibus-daemon -d -r -x"
   ];
+
+   # show ui app
+  programs.zsh.profileExtra = lib.mkAfter ''
+    rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
+    rm -rf ${config.home.homeDirectory}/.icons/nix-icons
+    ls ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop > ${config.home.homeDirectory}/.cache/current_desktop_files.txt
+  '';
+
+  home.activation = {
+    linkDesktopApplications = {
+      after = [ "writeBoundary" "createXdgUserDirectories" ];
+      before = [ ];
+      data = ''
+        rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
+        rm -rf ${config.home.homeDirectory}/.icons/nix-icons
+        mkdir -p ${config.home.homeDirectory}/.local/share/applications/home-manager
+        mkdir -p ${config.home.homeDirectory}/.icons
+        ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.icons/nix-icons
+
+        # Check if the cached desktop files list exists
+        if [ -f ${config.home.homeDirectory}/.cache/current_desktop_files.txt ]; then
+          current_files=$(cat ${config.home.homeDirectory}/.cache/current_desktop_files.txt)
+        else
+          current_files=""
+        fi
+
+        # Symlink new desktop entries
+        for desktop_file in ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop; do
+          if ! echo "$current_files" | grep -q "$(basename $desktop_file)"; then
+            ln -sf "$desktop_file" ${config.home.homeDirectory}/.local/share/applications/home-manager/$(basename $desktop_file)
+          fi
+        done
+
+        # Update desktop database
+        ${pkgs.desktop-file-utils}/bin/update-desktop-database ${config.home.homeDirectory}/.local/share/applications
+      '';
+    };
+  };
 }
 
