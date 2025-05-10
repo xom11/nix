315ffd76962206a453380621c83ee17d4d82f013
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-10 15:29:54 +0700

    update

--- home.nix
+++ home.nix
@@ -89,18 +89,6 @@
   programs.zsh = {
     enable = true;
     theme = "gentoo";
-    plugins = [
-      {
-          name = "powerlevel10k";
-          src = pkgs.zsh-powerlevel10k;
-          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-      {
-          name = "powerlevel10k-config";
-          src = lib.cleanSource ./config;
-          file = "p10k.zsh";
-      }
-    ];
     }; 
   };
 }
