2d6470fbf8521fbf27c94bbfe0d5ad8e5176f0a1
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-05-13 19:50:21 +0700

    update

--- nixos/test.nix
+++ nixos/test.nix
@@ -9,4 +9,10 @@
   
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+
+  i18n.inputMethod = {
+    enabled = true;
+    type = "ibus";
+    ibus.engines = with pkgs.ibus-engines; [bamboo];
+  };
 }
