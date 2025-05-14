f3ed1aa15579d0d5155a8a19eb897110b6b8d8a4
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-14 16:20:20 +0700

    update

--- modules/kitty/default.nix
+++ modules/kitty/default.nix
@@ -5,6 +5,21 @@
   # Enable the kitty program
   programs.kitty = {
     enable = true;
-    # settings = kittyConf;
+    keybindings ={
+      "ctrl+c" = "copy_or_interrupt";
+      "ctrl+v" = "paste-from-clipboard";
+    }
+    settings = {
+      font_family = "Fira Code";
+      font_size = 12.0;
+      background_opacity = 0.8;
+      cursor_shape = "block";
+      cursor_blink_interval = 500;
+      cursor_blink_while_typing = true;
+      background_opacity = 0.8;
+      enable_blur = 0.8;
+      remember_window_size = true;
+    };
+
   };
 }
