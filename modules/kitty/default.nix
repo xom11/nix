229380e3cffb9bad1ed92614016d111b303d2a07
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-14 16:33:51 +0700

    update

--- modules/kitty/default.nix
+++ modules/kitty/default.nix
@@ -1,28 +1,6 @@
 { pkgs, config, ... }:
 {
-  # Define the path to the kitty configuration file
-  
-  # Enable the kitty program
-  programs.kitty = {
-    enable = true;
-    keybindings ={
-      "ctrl+c" = "copy_or_interrupt";
-      "ctrl+v" = "paste-from-clipboard";
-    };
-    settings = {
-      font_family = "Fira Code";
-      font_size = 12.0;
-      cursor_shape = "block";
-      cursor_blink_interval = 500;
-      cursor_blink_while_typing = true;
-      background_opacity = 0.8;
-      enable_blur = 0.8;
-      remember_window_size = true;
-      tab_bar_style = "separator";
-      tab_separator = "|";
-      tab_title_template = " {index} {title} ";
-      tab_bar_edge = "top";
-    };
-
+  home.files."./.config/kitty" = {
+    source = ./kitty.conf;
   };
 }
