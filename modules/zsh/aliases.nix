342bad1f3828860253461627c1b77919e6871818
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-05-15 14:29:36 +0700

    update

--- modules/zsh/aliases.nix
+++ modules/zsh/aliases.nix
@@ -12,8 +12,6 @@
     spy = "source .venv/bin/activate";
     cc = "conda create -p ./.venv python==3.12";
     ca = "conda activate ./.venv";
-    gcg = "git config --global user.name khanhkhanhlele && git config --global user.email namkhanh20xx@gmail.com";
-    gcl = "git config --local user.name khanhkhanhlele && git config --local user.email namkhanh20xx@gmail.com";
     gu = "git pull && git add . && git commit -m \"update\" && git push";
     py = "python3";
     py310 = "python3.10";
