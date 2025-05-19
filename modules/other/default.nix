813eb2b4041755eabf11e70a8d3bfb4b83c59902
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-05-19 16:05:46 +0700

    update

--- modules/other/default.nix
+++ modules/other/default.nix
@@ -1,7 +1,6 @@
 { config, pkgs, inputs, ...}:
 {
   imports = [
-    ./fonts.nix
   ];
   home.packages = with pkgs;[
     wmctrl
--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -1,5 +1,7 @@
+{pkgs, lib, config, ...}:
 {
   fonts.packages = with pkgs; [
+    nerd-fonts.dejavu-sans-mono
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-emoji
@@ -9,5 +11,6 @@
     mplus-outline-fonts.githubRelease
     dina-font
     proggyfonts
+    meslo-lgs-nf
 ];
 }
