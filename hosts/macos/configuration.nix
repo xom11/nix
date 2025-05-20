102c8c816743865812e17c59ff4279f51dceeef7
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-20 19:21:50 +0700

    update

--- hosts/macos/configuration.nix
+++ hosts/macos/configuration.nix
@@ -1,5 +1,6 @@
 
 { config, pkgs, ... }:
+let user = builtins.getEnv "USER"; in
 
 {
   imports = [
@@ -13,5 +14,47 @@
   programs.zsh.enable = true; 
   nixpkgs.hostPlatform = "aarch64-darwin";
 
-  system.stateVersion = 6;
+  system = {
+    # Turn off NIX_PATH warnings now that we're using flakes
+    checks.verifyNixPath = false;
+    primaryUser = user;
+    stateVersion = 6;
+    defaults = {
+      # LaunchServices = {
+      #   LSQuarantine = false;
+      # };
+      # NSGlobalDomain = {
+      #   AppleShowAllExtensions = true;
+      #   ApplePressAndHoldEnabled = false;
+
+      #   # 120, 90, 60, 30, 12, 6, 2
+      #   KeyRepeat = 2;
+
+      #   # 120, 94, 68, 35, 25, 15
+      #   InitialKeyRepeat = 15;
+      #   "com.apple.mouse.tapBehavior" = 1;
+      #   "com.apple.sound.beep.volume" = 0.0;
+      #   "com.apple.sound.beep.feedback" = 0;
+      # };
+      dock = {
+        autohide = false;
+        # show-recents = false;
+        # launchanim = true;
+        # mouse-over-hilite-stack = true;
+        orientation = "bottom";
+        # tilesize = 48;
+      };
+    #   finder = {
+    #     _FXShowPosixPathInTitle = false;
+    #   };
+    #   trackpad = {
+    #     Clicking = true;
+    #     TrackpadThreeFingerDrag = true;
+    #   };
+    };
+    # keyboard = {
+    #   enableKeyMapping = true;
+    #   remapCapsLockToControl = true;
+    # };
+  };
 }
