b0fb4059bfc743b611200d525010d8ab9feb082a
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-05-20 14:31:31 +0700

    update

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -7,7 +7,7 @@ in
   imports =
     [ # Include the results of the hardware scan.
       /etc/nixos/configuration.nix
-      ./modules/fonts
+      ../../modules/fonts
     ];
   
   programs.zsh.enable = true;
