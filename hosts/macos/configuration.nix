0926a1d98771e5fcb9f0fecd66d0378b9932e90b
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-21 13:27:58 +0700

    update

--- hosts/macos/configuration.nix
+++ hosts/macos/configuration.nix
@@ -15,5 +15,7 @@ in
   nix.settings.experimental-features = "nix-command flakes";
   programs.zsh.enable = true; 
   nixpkgs.hostPlatform = "aarch64-darwin";
+  system.primaryUser = user;
+  system.stateVersion = 6;
 
 }
