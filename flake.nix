6df1cc440e2dbe6dced574ba053d753e2d8408a3
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-22 12:48:21 +0700

    update

--- flake.nix
+++ flake.nix
@@ -1,11 +1,6 @@
 {
   description = "Home Manager configuration";
 
-  # nixConfig = {
-  #   experimental-features = ["nix-command" "flakes"];
-  #   allowUnfree = true;
-  # };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
