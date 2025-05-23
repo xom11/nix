b481180a725871ba72dc72ec57600d50885c2693
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-05-23 12:04:55 +0700

    update

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -44,4 +44,10 @@ in
 
   security.sudo.wheelNeedsPassword = false;
 
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [username];
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
+
 }
