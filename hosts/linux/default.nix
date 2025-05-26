1ce27ec02bfcbf20b7841f133d2ee74da9d67db3
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-26 12:21:15 +0700

    update

--- hosts/linux/default.nix
+++ hosts/linux/default.nix
@@ -3,12 +3,16 @@ let
   bamboo = pkgs.callPackage ./ibus-bamboo.nix {};
 in
 {
+  config = {
+    nixpkgs.hostPlatform = "x86_64-linux";
+  };
   imports =
     [ # Include the results of the hardware scan.
     ];
   
   # programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+  
 
 
 
