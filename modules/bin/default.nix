afd7c52b294d88beec4e8c403539ba2077d56b81
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-26 14:22:28 +0700

    update

--- modules/bin/default.nix
+++ modules/bin/default.nix
@@ -1,8 +1,8 @@
 {pkgs,...}:
 {
 home.packages = with pkgs; [
-  (writeShellScriptBin "add-authkey" (builtins.readFile ./add-authkey)
-  (writeShellScriptBin "add-visudo" (builtins.readFile ./add-visudo)
-  (writeShellScriptBin "set-zsh" (builtins.readFile ./set-zsh)
+  (writeShellScriptBin "add-authkey" (builtins.readFile ./add-authkey))
+  (writeShellScriptBin "add-visudo" (builtins.readFile ./add-visudo))
+  (writeShellScriptBin "set-zsh" (builtins.readFile ./set-zsh))
   ];
 }
