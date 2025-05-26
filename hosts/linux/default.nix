95246700bdd2dc218d304cff42ac1d6a5b76a869
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-26 11:54:02 +0700

    update

--- hosts/linux/default.nix
+++ hosts/linux/default.nix
@@ -5,11 +5,6 @@
     nixpkgs.hostPlatform = "x86_64-linux";
 
     environment = {
-      etc = {
-        "foo.conf".text = ''
-          launch_the_rockets = true
-        '';
-      };
       systemPackages = [
         pkgs.ripgrep
         pkgs.fd
@@ -18,19 +13,5 @@
       ];
     };
 
-    systemd.services = {
-      foo = {
-        enable = true;
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-        };
-        wantedBy = [ "system-manager.target" ];
-        script = ''
-          ${lib.getBin pkgs.hello}/bin/hello
-          echo "We launched the rockets!"
-        '';
-      };
-    };
   };
 }
