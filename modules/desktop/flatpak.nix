dd33d24e70c21dc19104bae7ee7990b74924baf4
khanhkhanhlele <namkhanh2172@gmail.com>
2025-05-30 23:47:34 +0700

    update

--- modules/desktop/flatpak.nix
+++ modules/desktop/flatpak.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 {
   services.flatpak.packages = [
-    # { appId = "com.simplenote.Simplenote"; origin = "flathub"; }
+    { appId = "com.simplenote.Simplenote"; origin = "flathub"; }
     { appId = "com.sindresorhus.Caprine"; origin = "flathub"; }
   ];
 }
