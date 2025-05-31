eb64fd9b71bbb5d1f2918978182e1dc62bb524c2
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-05-31 15:19:45 +0700

    update

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daem
 . ~/.nix-profile/etc/profile.d/nix.sh
 export NIX_CONFIG="extra-experimental-features = nix-command flakes"
 export NIXPKGS_ALLOW_UNFREE=1
-nix run github:nix-community/home-manager -- switch --impure --flake "github:kln-os/nix/main#server" --refresh
+nix run github:nix-community/home-manager -- switch --impure --flake "github:kln-os/nix/main#local" --refresh
