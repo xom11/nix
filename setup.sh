15d02d5ec72a1fc5abbfac701c6bf829f41a8077
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-09 13:14:19 +0700

    update

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determin
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 nix run github:nix-community/home-manager -- switch --impure -b backup --flake "github:kln-os/nix/main#server" --refresh
 add-visudo && add-authkey && set-zsh
+
+nix run github:numtide/system-manager -- switch  --flake "~/nix"
