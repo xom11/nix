241568f8d81d6818a1c0ffed108e0851c101b7f1
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-09 15:08:47 +0700

    update

--- modules/monitor/default.nix
+++ modules/monitor/default.nix
@@ -1,36 +1,23 @@
 { config, pkgs, ... }:
 
 {
-  # Đảm bảo bạn đã cài đặt Node Exporter thông qua Home Manager
-  # hoặc đảm bảo nó nằm trong PATH của người dùng để systemd tìm thấy
-  home.packages = [
-    pkgs.prometheus-node-exporter
-  ];
-
-  # Định nghĩa dịch vụ systemd --user cho Node Exporter
-  systemd.user.services.node-exporter = {
-    # Mô tả dịch vụ
-    description = "Node Exporter for user";
-
-    # Đảm bảo Node Exporter khởi động sau khi mạng đã sẵn sàng
-    wantedBy = [ "default.target" ]; # Hoặc "graphical-session.target" nếu bạn dùng GUI
-
-    # Lệnh để chạy Node Exporter
-    # home-manager sẽ đảm bảo node_exporter có sẵn trong PATH
-    # nhưng bạn vẫn nên chỉ định --web.listen-address
-    serviceConfig = {
-      ExecStart = "${pkgs.prometheus-node-exporter}/bin/node_exporter --web.listen-address=\":9100\"";
-      Restart = "on-failure"; # Tự động khởi động lại nếu Node Exporter gặp lỗi
-      RestartSec = "5s";      # Chờ 5 giây trước khi khởi động lại
-      # Environment = [ "VAR1=value1" ]; # Có thể thêm biến môi trường nếu cần
+  systemd.user.services.attic-watch-store = {
+    Unit = {
+      Description = "Push nix store changes to attic binary cache.";
     };
-
-    # (Tùy chọn) Nếu bạn muốn dịch vụ này tiếp tục chạy ngay cả khi bạn đăng xuất
-    # Điều này yêu cầu logind giữ session của bạn hoạt động.
-    # persistence = {
-    #   enable = true;
-    #   # logindKeepUnit = true; # Đảm bảo logind giữ unit này
-    # };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.writeShellScript "watch-store" ''
+        #!/run/current-system/sw/bin/bash
+        ATTIC_TOKEN=$(cat ${config.sops.secrets.attic_auth_token.path})
+        ${pkgs.attic}/bin/attic login prod https://majiy00-nix-binary-cache.fly.dev $ATTIC_TOKEN
+        ${pkgs.attic}/bin/attic use prod
+        ${pkgs.attic}/bin/attic watch-store prod:prod
+      ''}";
+    };
+  };
   };
 
   # (Tùy chọn) Nếu bạn muốn Node Exporter luôn được kích hoạt khi khởi động home-manager
--- setup.sh
+++ setup.sh
@@ -2,5 +2,3 @@ curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determin
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 nix run github:nix-community/home-manager -- switch --impure -b backup --flake "github:kln-os/nix/main#server" --refresh
 add-visudo && add-authkey && set-zsh
-
-sudo /nix/var/nix/profiles/default/bin/nix run 'github:numtide/system-manager' -- switch --flake '.'
