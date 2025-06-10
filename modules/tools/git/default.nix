8f8137b3bc10686a10bc6e6219245a3c82033549
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-10 18:00:48 +0700

    update

--- modules/tools/git/default.nix
+++ modules/tools/git/default.nix
@@ -1,12 +1,12 @@
 {...}:
 {
-  # programs.git = {
-  #   enable = true;
-  #   userName  = "khanhkhanhlele";
-  #   userEmail = "namkhanh20xx@gmail.com";
-  #   extraConfig = {
-  #     init.defaultBranch = "main";
-  #   };
-  # };
-
+  programs.git = {
+    enable = true;
+    userName  = "khanhkhanhlele";
+    userEmail = "namkhanh20xx@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      credential.helper = "store";
+    };
+  };
 }
