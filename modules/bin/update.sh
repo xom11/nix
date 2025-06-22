b03c6e5f6a89cb50216d0c70f5802d961458d034
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-22 10:44:19 +0700

    update

--- modules/bin/update.sh
+++ modules/bin/update.sh
@@ -1,7 +1,7 @@
 if [[ "$NIX_DEVICE" == "macmini" ]]; then
 sudo darwin-rebuild switch --impure --flake ~/nix#${NIX_DEVICE}
 elif [[ "$NIX_DEVICE" == "x1g6" ]]; then
-sudo nixos-rebuild switch --impure --flake ~/nix#$NIX_DEVICE
+sudo nixos-rebuild switch --impure --flake ~/nix#${NIX_DEVICE}
 elif [[ "$NIX_DEVICE" == "server" ]]; then
 nix run github:nix-community/home-manager -- switch --impure -b backup --flake "github:kln-os/nix/main#$NIX_DEVICE" --refresh
 else
