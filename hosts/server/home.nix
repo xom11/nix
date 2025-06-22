635459fe303ae19b5af11873ad7bd81e45f56dbe
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-22 11:45:50 +0700

    update

--- hosts/server/home.nix
+++ hosts/server/home.nix
@@ -23,6 +23,6 @@ in
   ];
   home.shellAliases = {
     update = "nix run github:nix-community/home-manager -- switch --impure -b backup --refresh --flake 'github:kln-os/nix/main#server' ";
-  }
+  };
 }
 
