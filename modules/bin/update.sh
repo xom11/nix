4d35ea2b0e12832774ee55bd8a9192812c432a88
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-22 10:44:50 +0700

    update

--- modules/bin/update.sh
+++ modules/bin/update.sh
@@ -3,7 +3,7 @@ sudo darwin-rebuild switch --impure --flake ~/nix#${NIX_DEVICE}
 elif [[ "$NIX_DEVICE" == "x1g6" ]]; then
 sudo nixos-rebuild switch --impure --flake ~/nix#${NIX_DEVICE}
 elif [[ "$NIX_DEVICE" == "server" ]]; then
-nix run github:nix-community/home-manager -- switch --impure -b backup --flake "github:kln-os/nix/main#$NIX_DEVICE" --refresh
+nix run github:nix-community/home-manager -- switch --impure -b backup --flake "github:kln-os/nix/main#${NIX_DEVICE}" --refresh
 else
 echo "Unknown NIX_DEVICE: $NIX_DEVICE"
 fi
