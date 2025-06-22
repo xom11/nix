2b5964ee4a26cf083df889ae363635fb6ede12d9
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-22 11:16:26 +0700

    update

--- modules/bin/default.nix
+++ modules/bin/default.nix
@@ -5,6 +5,5 @@ home.packages = with pkgs; [
   (writeShellScriptBin "add-visudo" (builtins.readFile ./add-visudo.sh))
   (writeShellScriptBin "set-zsh" (builtins.readFile ./set-zsh.sh))
   (writeShellScriptBin "install-docker" (builtins.readFile ./install-docker.sh))
-  (writeShellScriptBin "update" (builtins.readFile ./update.sh))
   ];
 }
