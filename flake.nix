343ccffd00209515f8c2e8a5295fe3dea924afa5
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-23 12:48:34 +0700

    update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
+    # systems.url = "github:nix-systems/default";
 
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
@@ -75,7 +75,7 @@
     };
     homeConfigurations = {
       "server" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = nixpkgs.legacyPackages.${pkgs.system};
         modules = [
           ./hosts/server/home.nix
         ];
