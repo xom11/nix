437634234de8dea03060a2f05e16423c7021be14
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-23 18:26:35 +0700

    update

--- hosts/x1g6/configs/apps.nix
+++ hosts/x1g6/configs/apps.nix
@@ -1,13 +1,9 @@
-{pkgs, ...}:
+{...}:
 {
   virtualisation.docker.enable = true;
-  boot.kernelModules = [ "kvm-intel" "kvm-amd" ];
-  # virtualisation.libvirtd.enable = true;
+  users.users.${username}.extraGroups = [ "docker" ];
 
   services.tailscale.enable = true;
   services.openssh.enable = true;
-  programs.virt-manager.enable = true;
-  users.groups.libvirtd.members = ["kln"];
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
+
 }
