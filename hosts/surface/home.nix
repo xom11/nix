e0728774a939696160716cbcf78e7b0cd77531eb
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-24 11:19:13 +0700

    update

--- hosts/surface/home.nix
+++ hosts/surface/home.nix
@@ -38,36 +38,36 @@ in
     ls ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop > ${config.home.homeDirectory}/.cache/current_desktop_files.txt
   '';
 
-  # home.activation = {
-  #   linkDesktopApplications = {
-  #     after = [ "writeBoundary" "createXdgUserDirectories" ];
-  #     before = [ ];
-  #     data = ''
-  #       rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
-  #       rm -rf ${config.home.homeDirectory}/.icons/nix-icons
-  #       mkdir -p ${config.home.homeDirectory}/.local/share/applications/home-manager
-  #       mkdir -p ${config.home.homeDirectory}/.icons
-  #       ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.icons/nix-icons
+  home.activation = {
+    linkDesktopApplications = {
+      after = [ "writeBoundary" "createXdgUserDirectories" ];
+      before = [ ];
+      data = ''
+        rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
+        rm -rf ${config.home.homeDirectory}/.icons/nix-icons
+        mkdir -p ${config.home.homeDirectory}/.local/share/applications/home-manager
+        mkdir -p ${config.home.homeDirectory}/.icons
+        ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.icons/nix-icons
 
-  #       # Check if the cached desktop files list exists
-  #       if [ -f ${config.home.homeDirectory}/.cache/current_desktop_files.txt ]; then
-  #         current_files=$(cat ${config.home.homeDirectory}/.cache/current_desktop_files.txt)
-  #       else
-  #         current_files=""
-  #       fi
+        # Check if the cached desktop files list exists
+        if [ -f ${config.home.homeDirectory}/.cache/current_desktop_files.txt ]; then
+          current_files=$(cat ${config.home.homeDirectory}/.cache/current_desktop_files.txt)
+        else
+          current_files=""
+        fi
 
-  #       # Symlink new desktop entries
-  #       for desktop_file in ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop; do
-  #         if ! echo "$current_files" | grep -q "$(basename $desktop_file)"; then
-  #           ln -sf "$desktop_file" ${config.home.homeDirectory}/.local/share/applications/home-manager/$(basename $desktop_file)
-  #         fi
-  #       done
+        # Symlink new desktop entries
+        for desktop_file in ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop; do
+          if ! echo "$current_files" | grep -q "$(basename $desktop_file)"; then
+            ln -sf "$desktop_file" ${config.home.homeDirectory}/.local/share/applications/home-manager/$(basename $desktop_file)
+          fi
+        done
 
-  #       # Update desktop database
-  #       ${pkgs.desktop-file-utils}/bin/update-desktop-database ${config.home.homeDirectory}/.local/share/applications
-  #     '';
-  #   };
-  # };
+        # Update desktop database
+        ${pkgs.desktop-file-utils}/bin/update-desktop-database ${config.home.homeDirectory}/.local/share/applications
+      '';
+    };
+  };
 
   programs.home-manager.enable = true;
 
