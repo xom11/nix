60b283f19f6472103d946c0fc30caabfd449e2b5
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-24 12:25:52 +0700

    update

--- modules/apps/ubuntu/default.nix
+++ modules/apps/ubuntu/default.nix
@@ -1,8 +1,8 @@
 {...}:
 {
-  home.activation = {
-    installApps = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      run ${./install.sh}
-    ''
-  }
+  # home.activation = {
+  #   installApps = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  #     run ${./install.sh}
+  #   ''
+  # }
 }
