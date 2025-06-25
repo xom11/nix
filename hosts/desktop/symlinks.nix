946efae582fee87c08fa23ced10a1bec64930b21
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-25 17:45:42 +0700

    update

--- hosts/desktop/symlinks.nix
+++ hosts/desktop/symlinks.nix
@@ -5,10 +5,10 @@
       after = [ "writeBoundary" "createXdgUserDirectories" ];
       before = [ ];
       data = ''
-        # mkdir -p ${config.home.homeDirectory}/.local/share/applications/nix-applications
         mkdir -p ${config.home.homeDirectory}/.local/share/icons/nix-icons
         ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.local/share/icons/nix-icons
 
+        mkdir -p ${config.home.homeDirectory}/.local/share/applications
         for file in ${config.home.homeDirectory}/.nix-profile/share/applications/*; do
             if [ -f "$file" ]; then
                 ln -sf "$file" ${config.home.homeDirectory}/.local/share/applications/
