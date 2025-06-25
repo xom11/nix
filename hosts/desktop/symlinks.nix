265b699c7ce00a984c4cb9a8373dfc0ef2f3853b
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-25 17:14:50 +0700

    update

--- hosts/desktop/symlinks.nix
+++ hosts/desktop/symlinks.nix
@@ -5,27 +5,11 @@
       after = [ "writeBoundary" "createXdgUserDirectories" ];
       before = [ ];
       data = ''
-        rm -rf ${config.home.homeDirectory}/.local/share/applications/home-manager
-        rm -rf ${config.home.homeDirectory}/.icons/nix-icons
-        mkdir -p ${config.home.homeDirectory}/.local/share/applications/home-manager
-        mkdir -p ${config.home.homeDirectory}/.icons
-        ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.icons/nix-icons
 
-        # Check if the cached desktop files list exists
-        if [ -f ${config.home.homeDirectory}/.cache/current_desktop_files.txt ]; then
-          current_files=$(cat ${config.home.homeDirectory}/.cache/current_desktop_files.txt)
-        else
-          current_files=""
-        fi
-
-        # Symlink new desktop entries
-        for desktop_file in ${config.home.homeDirectory}/.nix-profile/share/applications/*.desktop; do
-          if ! echo "$current_files" | grep -q "$(basename $desktop_file)"; then
-            ln -sf "$desktop_file" ${config.home.homeDirectory}/.local/share/applications/home-manager/$(basename $desktop_file)
-          fi
-        done
 
         ln -sf ${config.home.homeDirectory}/.nix-profile/share/gnome-shell/extensions ${config.home.homeDirectory}/.local/share/gnome-shell/
+        ln -sf ${config.home.homeDirectory}/.nix-profile/bin ${config.home.homeDirectory}/.local
+        ln -sf ${config.home.homeDirectory}/.nix-profile/share/applications ${config.home.homeDirectory}/.local/share
 
         ${pkgs.desktop-file-utils}/bin/update-desktop-database ${config.home.homeDirectory}/.local/share/applications
       '';
