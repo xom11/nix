285dfd88e4cfbf1b32be25a30feeabe3d4bf43c2
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-25 17:43:34 +0700

    update

--- hosts/desktop/symlinks.nix
+++ hosts/desktop/symlinks.nix
@@ -5,12 +5,18 @@
       after = [ "writeBoundary" "createXdgUserDirectories" ];
       before = [ ];
       data = ''
+        # mkdir -p ${config.home.homeDirectory}/.local/share/applications/nix-applications
+        mkdir -p ${config.home.homeDirectory}/.local/share/icons/nix-icons
+        ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.local/share/icons/nix-icons
 
+        for file in ${config.home.homeDirectory}/.nix-profile/share/applications/*; do
+            if [ -f "$file" ]; then
+                ln -sf "$file" ${config.home.homeDirectory}/.local/share/applications/
+            fi
+        done
 
         mkdir -p ${config.home.homeDirectory}/.local/share/gnome-shell
         ln -sf ${config.home.homeDirectory}/.nix-profile/share/gnome-shell/extensions ${config.home.homeDirectory}/.local/share/gnome-shell/
-        ln -sf ${config.home.homeDirectory}/.nix-profile/bin ${config.home.homeDirectory}/.local
-        ln -sf ${config.home.homeDirectory}/.nix-profile/share/applications ${config.home.homeDirectory}/.local/share
 
         ${pkgs.desktop-file-utils}/bin/update-desktop-database ${config.home.homeDirectory}/.local/share/applications
       '';
