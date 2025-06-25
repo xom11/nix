c85865cd568c7aba901f81cd045b5794985f8c04
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-25 11:36:24 +0700

    update

--- hosts/desktop/symlinks.nix
+++ hosts/desktop/symlinks.nix
@@ -8,7 +8,11 @@
         mkdir -p ${config.home.homeDirectory}/.local/share/applications/nix-applications
         mkdir -p ${config.home.homeDirectory}/.local/share/icons/nix-icons
         ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.local/share/icons/nix-icons
-        find "${config.home.homeDirectory}/.nix-profile/share/applications" -type f -exec ln -sf {} "${config.home.homeDirectory}/.local/share/applications/" \;
+        for file in ${config.home.homeDirectory}/.nix-profile/share/applications/*; do
+            if [ -f "$file" ]; then
+                ln -sf "$file" ${config.home.homeDirectory}/.local/share/applications/
+            fi
+        done
 
         ${pkgs.desktop-file-utils}/bin/update-desktop-database ${config.home.homeDirectory}/.local/share/applications
       '';
