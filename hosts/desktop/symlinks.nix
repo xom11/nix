fdec3c74a5ee0ba72d3b3327da09b3d6952be33d
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-25 11:11:04 +0700

    update

--- hosts/desktop/symlinks.nix
+++ hosts/desktop/symlinks.nix
@@ -8,7 +8,7 @@
         mkdir -p ${config.home.homeDirectory}/.local/share/applications/nix-applications
         mkdir -p ${config.home.homeDirectory}/.local/share/icons/nix-icons
         ln -sf ${config.home.homeDirectory}/.nix-profile/share/icons ${config.home.homeDirectory}/.local/share/icons/nix-icons
-        ln -sf ${config.home.homeDirectory}/.nix-profile/share/application ${config.home.homeDirectory}/.local/share/applications/nix-applications
+        ln -sf ${config.home.homeDirectory}/.nix-profile/share/applications ${config.home.homeDirectory}/.local/share/applications/nix-applications
 
         ${pkgs.desktop-file-utils}/bin/update-desktop-database ${config.home.homeDirectory}/.local/share/applications
       '';
