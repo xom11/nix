9622d59844150d7215830fbd967d8cd12275cc49
Lê Nam Khánh <55955273+khanhkhanhlele@users.noreply.github.com>
2025-06-27 01:15:00 +0700

    upa

--- modules/apps/linux/update-nix-app.sh
+++ modules/apps/linux/update-nix-app.sh
@@ -1,3 +1,5 @@
-rsync -a ~/.nix-profile/share/applications/ ~/.local/share/applications 
 sudo rsync -a ~/.nix-profile/bin/ /usr/share/bin/
+sudo mkdir -p /usr/share/icons/nix/
+sudo rsync -a ~/.nix-profile/share/applications/ /usr/share/applications/
+sudo rsync -a ~/.nix-profile/share/icons/ /usr/share/icons/nix/
 sudo update-desktop-database
