46c12bb624278a9da2bdb78f261a495043658728
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-26 10:45:09 +0700

    update

--- modules/apps/linux/default.nix
+++ modules/apps/linux/default.nix
@@ -9,6 +9,18 @@
     (config.lib.nixGL.wrap kitty)
     # (config.lib.nixGL.wrap brave)
     # (config.lib.nixGL.wrap vscode)
+    preload
+    bitwarden-desktop
+    discord
+    vscode
+    telegram-desktop
+    localsend
+    joplin-desktop
+    slack
+    thunderbird
+    google-chrome
+    # chromedriver
+    caprine
 
   ];
 }
