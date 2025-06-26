14cfe8fd6813aa4e236c90a43579de62770d96a0
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-26 09:45:04 +0700

    update

--- modules/i18n/default.nix
+++ modules/i18n/default.nix
@@ -1,9 +1,8 @@
 {pkgs, ...}:{
-  i18n.inputMethod.fcitx5 = {
-    addons = with pkgs; 
-    [
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
       fcitx5-bamboo
     ];
-
   };
 }
