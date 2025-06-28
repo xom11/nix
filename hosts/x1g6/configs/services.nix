ed9a641eef4d20e1ae2495ffb408d5aaf31fc3ca
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-28 23:06:06 +0700

    update

--- hosts/x1g6/configs/services.nix
+++ hosts/x1g6/configs/services.nix
@@ -12,4 +12,16 @@
   #   ibus.engines = with pkgs.ibus-engines; [bamboo];
   # };
 
+  services.keyd = {
+    enable = true;
+  };
+
+  # Optional, but makes sure that when you type the make palm rejection work with keyd
+  # https://github.com/rvaiya/keyd/issues/723
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [Serial Keyboards]
+    MatchUdevType=keyboard
+    MatchName=keyd virtual keyboard
+    AttrKeyboardIntegration=internal
+  '';
 }
