b0e9d326c49007721bffca0d62aad584ff90c71f
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-28 23:09:59 +0700

    update

--- hosts/x1g6/configs/services.nix
+++ hosts/x1g6/configs/services.nix
@@ -12,9 +12,26 @@
   #   ibus.engines = with pkgs.ibus-engines; [bamboo];
   # };
 
-  services.keyd = {
-    enable = true;
+services.keyd = {
+  enable = true;
+  keyboards = {
+    # The name is just the name of the configuration file, it does not really matter
+    default = {
+      ids = [ "*" ]; # what goes into the [id] section, here we select all keyboards
+      # Everything but the ID section:
+      settings = {
+        # The main layer, if you choose to declare it in Nix
+        main = {
+          capslock = "layer(control)"; # you might need to also enclose the key in quotes if it contains non-alphabetical symbols
+        };
+        otherlayer = {};
+      };
+      extraConfig = ''
+        # put here any extra-config, e.g. you can copy/paste here directly a configuration, just remove the ids part
+      '';
+    };
   };
+};
 
   # Optional, but makes sure that when you type the make palm rejection work with keyd
   # https://github.com/rvaiya/keyd/issues/723
