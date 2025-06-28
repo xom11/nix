6b49868d8d03fd76ac423742978d54ed95a1148c
khanhkhanhlele <namkhanh2172@gmail.com>
2025-06-28 12:27:40 +0700

    update

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -5,32 +5,32 @@
     nixpkgs.hostPlatform = "x86_64-linux";
     system-manager.allowAnyDistro = true;
 
-    # environment = {
-    #   etc = {
-    #     "foo.conf".text = ''
-    #       launch_the_rockets = true
-    #     '';
-    #   };
-    #   systemPackages = [
-    #     pkgs.ripgrep
-    #     pkgs.fd
-    #     pkgs.hello
-    #   ];
-    # };
+    environment = {
+      etc = {
+        "foo.conf".text = ''
+          launch_the_rockets = true
+        '';
+      };
+      systemPackages = [
+        pkgs.ripgrep
+        pkgs.fd
+        pkgs.hello
+      ];
+    };
 
-    # systemd.services = {
-    #   foo = {
-    #     enable = true;
-    #     serviceConfig = {
-    #       Type = "oneshot";
-    #       RemainAfterExit = true;
-    #     };
-    #     wantedBy = [ "system-manager.target" ];
-    #     script = ''
-    #       ${lib.getBin pkgs.hello}/bin/hello
-    #       echo "We launched the rockets!"
-    #     '';
-    #   };
-    # };
+    systemd.services = {
+      foo = {
+        enable = true;
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+        };
+        wantedBy = [ "system-manager.target" ];
+        script = ''
+          ${lib.getBin pkgs.hello}/bin/hello
+          echo "We launched the rockets!"
+        '';
+      };
+    };
   };
 }
