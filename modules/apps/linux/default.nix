8a3be36a00d7467bc2af2c38656dce8eb9a7b3d7
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-01 10:05:15 +0700

    update

--- modules/apps/linux/default.nix
+++ modules/apps/linux/default.nix
@@ -11,19 +11,17 @@
   home.packages = with pkgs; [
     (writeShellScriptBin "update-nix-app" (builtins.readFile ./update-nix-app.sh))
     (config.lib.nixGL.wrap kitty)
-    # (config.lib.nixGL.wrap brave)
     brave
-    # (config.lib.nixGL.wrap discord)
-    # (config.lib.nixGL.wrap vscode)
-    # (config.lib.nixGL.wrap google-chrome)
-    # bitwarden-desktop
-    # telegram-desktop
-    # localsend
-    # joplin-desktop
-    # slack
-    # thunderbird
-    # google-chrome
-    # chromedriver
-    # caprine
+    discord
+    vscode
+    bitwarden-desktop
+    telegram-desktop
+    localsend
+    joplin-desktop
+    slack
+    thunderbird
+    google-chrome
+    chromedriver
+    caprine
   ];
 }
