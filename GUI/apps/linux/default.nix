e33511d30e676ea5c629fef24ee339c28b08775d
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-03 13:12:46 +0700

    update

--- GUI/apps/linux/default.nix
+++ GUI/apps/linux/default.nix
@@ -9,8 +9,8 @@
     (config.lib.nixGL.wrap kitty)
     (config.lib.nixGL.wrap localsend)
     telegram-desktop
-    # brave
-    # vscode
+    brave
+    vscode
 
     ####
     # (config.lib.nixGL.wrap vscode)
