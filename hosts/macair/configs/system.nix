95ccff19f8c07b2f6c0dd013da2bee9ee260d86a
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-12 15:15:19 +0700

    update

--- hosts/macair/configs/system.nix
+++ hosts/macair/configs/system.nix
@@ -29,7 +29,7 @@
       };
       trackpad = {
         Clicking = true;
-        Dragging = true:
+        Dragging = true;
       };
       # finder = {
       #   _FXShowPosixPathInTitle = false;
