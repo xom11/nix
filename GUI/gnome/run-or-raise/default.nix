99089b6d5aefa9685d95613d14e60f58a374b0fa
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-07-13 02:15:48 +0700

    update

--- GUI/gnome/run-or-raise/default.nix
+++ GUI/gnome/run-or-raise/default.nix
@@ -1,14 +1,14 @@
 {config, pkgs, lib, ...}:
 let
   RunOrRaisePath = "${config.home.homeDirectory}/.config/run-or-raise";
-  RunOrRaiseText = pkgs.writeText "tmp" (builtins.readFile ./shortcuts.conf);
+  RunOrRaiseFile = pkgs.writeText "tmp" (builtins.readFile ./shortcuts.conf);
 in
 {
   home.activation = {
     copyRunOrRaise =  lib.hm.dag.entryAfter ["writeBoundary"] ''
       rm -rf ${RunOrRaisePath}; 
       mkdir -p ${RunOrRaisePath};
-      cp ${RunOrRaise} ${RunOrRaisePath}/shortcuts.conf;
+      cp ${RunOrRaiseFile} ${RunOrRaisePath}/shortcuts.conf;
     '';
 };
 }
