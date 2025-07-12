467dac2d468aac7463ffb194ff1951a5803c3ebb
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-07-13 03:12:47 +0700

    update

--- CLI/programs/ssh/default.nix
+++ CLI/programs/ssh/default.nix
@@ -20,10 +20,5 @@
         ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
       fi
     '';
-    testABC = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      echo "SSH setup completed successfully."
-      sudo chmod 700 ~/.ssh 
-    '';
-
   };
 }
