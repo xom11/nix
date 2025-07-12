e7d427c11d9a2c5ed8018f189252fd0550e020be
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-12 14:48:50 +0700

    update

--- CLI/vm/default.nix
+++ CLI/vm/default.nix
@@ -1,9 +1,9 @@
 { config, pkgs, option, ... }:
 {
   home.packages = with pkgs; [
-    prometheus-node-exporter
-    redis
-    minio
+    # prometheus-node-exporter
+    # redis
+    # minio
     ffmpeg
     minio-client
     discordchatexporter-cli
