7ae9096197ffcf12efc3848fb20d0449fe938f6b
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-12 17:11:08 +0700

    update

--- hosts/macair/configs/default.nix
+++
@@ -1,7 +0,0 @@
-{...}:
-{
-  imports = [
-    ./system.nix
-    ./other.nix
-  ];
-}
--- hosts/macair/configuration.nix
+++ hosts/macair/configuration.nix
@@ -1,45 +1,17 @@
-{ config, pkgs, lib, username, hostname,... }:
+{ config, pkgs, lib, username, ... }:
+let 
+  hostname = "macair-qphus";
+in
 {
-  # enable flakes globally
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-
-  nix.package = pkgs.nix;
-
-  # do garbage collection weekly to keep disk usage low
-  nix.gc = {
-    automatic = lib.mkDefault true;
-    options = lib.mkDefault "--delete-older-than 7d";
-  };
-
-  # Disable auto-optimise-store because of this issue:
-  #   https://github.com/NixOS/nix/issues/7273
-  # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
-  nix.settings = {
-    auto-optimise-store = false;
-  };
+ 
   imports = [
-    ./configs
+    ../../MAC/base
+    ../../MAC/system
   ];
-
-  programs.zsh.enable = true; 
-  nixpkgs.hostPlatform = "aarch64-darwin";
-  system.primaryUser = username;
-  system.stateVersion = 6;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${username} = {
-    home = "/Users/${username}";
-    description = username;
+  networking = {
+    computerName = hostname;
+    hostName = hostname;
+    localHostName = hostname;
   };
-
-  security.sudo.extraConfig = ''
-    ${username} ALL=(ALL) NOPASSWD: ALL
-  '';
-
-  nix.settings.trusted-users = [username];
 
 }
