6bf49e3af72b848518e6867663c1e059203b4b44
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-07-13 01:30:06 +0700

    update

--- GUI/gnome/run-or-raise/default.nix
+++ GUI/gnome/run-or-raise/default.nix
@@ -1,12 +1,16 @@
 {config, ...}:
 {
   # xdg.configFile."run-or-raise/shortcut.conf".source = ./shortcuts.conf;
-  xdg.configFile."run-or-raise/_shortcuts.conf" = {
-    text = builtins.readFile ./shortcuts.conf; 
-    onChange = ''
-      rm -f ${config.xdg.configHome}/run-or-rise/shortcut.conf
-      cp ${config.xdg.configHome}/run-or-rise/_shortcuts.conf ${config.xdg.configHome}/run-or-rise/shortcut.conf
-      chmod u+w ${config.xdg.configHome}/run-or-rise/shortcut.conf
-    '';
+  # xdg.configFile."run-or-raise/_shortcuts.conf" = {
+  #   text = builtins.readFile ./shortcuts.conf; 
+  #   onChange = ''
+  #     rm -f ${config.xdg.configHome}/run-or-rise/shortcut.conf
+  #     cp ${config.xdg.configHome}/run-or-rise/_shortcuts.conf ${config.xdg.configHome}/run-or-rise/shortcut.conf
+  #     chmod u+w ${config.xdg.configHome}/run-or-rise/shortcut.conf
+  #   '';
+  # };
+  home.file."text.txt" = {
+    text = "";
+    onChange = "echo TEST";
   };
 }
