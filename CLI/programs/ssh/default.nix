5433725a0b113d0cb523293144b7a664f3c79dfd
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-07-13 02:57:46 +0700

    update

--- CLI/programs/ssh/default.nix
+++ CLI/programs/ssh/default.nix
@@ -3,20 +3,20 @@
   home.activation = {
 
     copySshConfig =  lib.hm.dag.entryAfter ["writeBoundary"] ''
-      rm -rf  ${config.home.homeDirectory}/.ssh/config; 
-      mkdir -p  ${config.home.homeDirectory}/.ssh;
-      cp ${./config}  ${config.home.homeDirectory}/.ssh/config;
+      rm -rf  ~/.ssh/config; 
+      mkdir -p  ~/.ssh;
+      cp ${./config}  ~/.ssh/config;
     '';
 
     copyAuthorizedKeys = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      rm -rf ${config.home.homeDirectory}/.ssh/authorized_keys;
-      mkdir -p ${config.home.homeDirectory}/.ssh;
-      cp ${./authorized_keys} "${config.home.homeDirectory}/.ssh/authorized_keys";
+      rm -rf ~/.ssh/authorized_keys;
+      mkdir -p ~/.ssh;
+      cp ${./authorized_keys} "~/.ssh/authorized_keys";
     '';
 
     genSshKey = lib.hm.dag.entryAfter ["copySshConfig"] ''
-      if [ ! -f "${config.home.homeDirectory}/.ssh/id_ed25519" ]; then
-        ssh-keygen -t ed25519 -f ${config.home.homeDirectory}/.ssh/id_ed25519 -N ""
+      if [ ! -f "~/.ssh/id_ed25519" ]; then
+        ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
       fi
     '';
 
