df24af4cb4e7a92dfcd05528b8a46837076c85b4
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-16 13:38:11 +0700

    update

--- LINUX/base/default.nix
+++ LINUX/base/default.nix
@@ -1,5 +1,6 @@
 {input, config, pkgs, lib, ... }:
 {
   nixpkgs.hostPlatform = "x86_64-linux";
-  # system-manager.allowAnyDistro = true;
+  system-manager.allowAnyDistro = true;
+  environment.etc."profile.d/system-manager-path.sh".enable = false
 }
