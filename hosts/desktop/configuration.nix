2431162a4cf06fabe7c9b6f08ac05fb84486d512
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-16 14:43:03 +0700

    update

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -5,7 +5,7 @@
     ../../LINUX/base
     ../../LINUX/services
   ];
-  home.file.".config/environment.d/system-manager-path.conf".text= ''
-    PATH="$HOME/.nix-profile/bin:$PATH"
+  environment.etc."environment.d/system-manager-path.conf".text= ''
+    PATH="/run/system-manager/sw/bin/:$PATH"
   '';
 }
