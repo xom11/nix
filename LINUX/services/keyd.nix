c7ace95f029f1b6ffcd0349ace6a4e09281f02ee
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-16 13:15:34 +0700

    update

--- LINUX/services/keyd.nix
+++ LINUX/services/keyd.nix
@@ -9,12 +9,6 @@
         capslock=overload(hyper, esc)
         [hyper:C-M-A]
       '';
-      "libinput/local-overrides.quirks".text = ''
-        [Serial Keyboards]
-        MatchUdevType=keyboard
-        MatchName=keyd virtual keyboard
-        AttrKeyboardIntegration=internal
-      '';
     };
   };
   # systemd.services = {
