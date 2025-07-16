f36765e0436d43cfae25e4783ac1db3d96b6e510
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-16 11:43:59 +0700

    update

--- LINUX/services/default.nix
+++ LINUX/services/default.nix
@@ -5,6 +5,11 @@
       "foo.conf".text = ''
         launch_the_rockets = true
       '';
+      "keyd/config".text = ''
+        [general]
+        default_layout = "us"
+        default_variant = "dvorak"
+      '';
     };
     systemPackages = [
       pkgs.ripgrep
