b2b3afc3b13f54230b50f9caf23ce48f11eb0ba3
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-16 10:42:48 +0700

    update

--- CLI/services/default.nix
+++ CLI/services/default.nix
@@ -1,8 +1,10 @@
 {pkgs, ...}:
 {
   # services.ssh-agent.enable = true;
+  home.packages = with pkgs; [
+    shadow
+  ];
   services.podman = {
     enable = true;
-    extraPackages = with pkgs; [ shadow ];
   };
 }
