e18cf81f515e18f698d36ee240a07e6fb358f6a1
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-16 11:49:13 +0700

    update

--- LINUX/services/default.nix
+++ LINUX/services/default.nix
@@ -5,11 +5,6 @@
       "foo.conf".text = ''
         launch_the_rockets = true
       '';
-      "keyd/config".text = ''
-        [general]
-        default_layout = "us"
-        default_variant = "dvorak"
-      '';
     };
     systemPackages = [
       pkgs.ripgrep
---
+++ LINUX/services/keyd.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+{
+  environment = {
+    etc = {
+      "keyd/default.conf".text = ''
+        [main]
+        capslock=overload(hyper, esc)
+        [hyper:C-M-A]
+      '';
+    };
+    # systemPackages = [
+  }
+
+}
