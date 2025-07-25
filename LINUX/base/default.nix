812a5f0fe7f79eec0e84884d9822cdbeb13b4e07
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-25 18:01:04 +0700

    update

--- LINUX/base/default.nix
+++ LINUX/base/default.nix
@@ -5,4 +5,7 @@
   environment.etc."environment.d/system-manager-path.conf".text= ''
     PATH="/run/system-manager/sw/bin/:$PATH"
   '';
+  environment.etc = {
+    "profile.d/system-manager-path.sh".enable = false
+  }
 }
