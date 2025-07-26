b28aa08cde238ea7910dc677a1eaed854058e700
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-07-26 22:02:10 +0700

    update

--- hosts/x1g6/home.nix
+++ hosts/x1g6/home.nix
@@ -32,6 +32,6 @@
   };
 
   home.shellAliases = {
-    update = "sudo nixos-rebuild switch --impure --refresh --flake github:kln-os/nix/main#x1g6";
+    update = "sudo nixos-rebuild switch --impure --refresh --flake ~/nix#x1g6";
   };
 }
