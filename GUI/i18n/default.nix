7727aba3826a6644adf7c7caa95c5af43feacb70
khanhkhanhlele <namkhanh2172@gmail.com>
2025-07-27 10:44:34 +0700

    update

--- GUI/i18n/default.nix
+++ GUI/i18n/default.nix
@@ -9,6 +9,16 @@
         # libsForQt5.fcitx5-unikey
       ];
       waylandFrontend = true; 
+      settings.inputMethod = {
+        GroupOrder."0" = "Default";
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "us";
+          DefaultIM = "bamboo";
+        };
+        "Groups/0/Items/0".Name = "keyboard-us";
+        "Groups/0/Items/1".Name = "bamboo";
+      };
     };
   };
   home.packages = with pkgs; [
