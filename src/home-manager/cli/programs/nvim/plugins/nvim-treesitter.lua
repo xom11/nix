3a19100c52767df92a6acbb13050bd64222de76c
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-04 10:59:25 +0700

    update

--- src/home-manager/cli/programs/nvim/plugins/nvim-treesitter.lua
+++ src/home-manager/cli/programs/nvim/plugins/nvim-treesitter.lua
@@ -1,23 +1,5 @@
 require("nvim-treesitter.configs").setup({
-    -- auto install
-    auto_install = true,
-    -- add language you want to highlight in code
-    ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "javascript",
-        "typescript",
-        "tsx",
-        "html",
-        "go",
-        "gomod",
-        "gowork",
-        "gosum",
-        "java",
-        "json",
-        "zig",
-    },
+    auto_install = false,
     sync_install = false,
     highlight = { enable = true },
     indent = { enable = true },
