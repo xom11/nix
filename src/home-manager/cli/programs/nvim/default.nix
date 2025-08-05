648861173bf1ae3582c6ee37f57406014d8a1159
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-05 13:40:15 +0700

    update

--- src/home-manager/cli/programs/nvim/default.nix
+++ src/home-manager/cli/programs/nvim/default.nix
@@ -1,10 +1,38 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./treesitter.nix
+
+  ];
   programs.nixvim = {
     enable = true;
+    globals.mapleader = " ";
 
     colorschemes.catppuccin.enable = true;
-    plugins.lualine.enable = true;
+
+    plugins = {
+      nix.enable = true;
+      lualine.enable = true;
+
+      lsp-lines.enable = true;
+      lspkind.enable = true;
+
+      neogit.enable = true;
+      cmp-zsh.enable = true;
+      noice.enable = true;
+      colorizer.enable = true;
+      luasnip.enable = true;
+
+      notify.enable = true; 
+
+      airline = {
+        enable = true;
+        #powerline = true;
+        settings = {
+          theme = "catppuccin";
+        };
+      };
+    };
   };
 }
