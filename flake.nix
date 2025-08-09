8a71fb2acf5cc93cce54f2274862f26186688196
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-09 18:11:55 +0700

    update

--- flake.nix
+++ flake.nix
@@ -54,31 +54,9 @@
       specialArgs =
         inputs
         // {
-          inherit username dotfileDir system homeDir pkgs;
+          inherit username dotfileDir system homeDir ;
         };
-      myapp = { poetry2nix, lib }: poetry2nix.mkPoetryApplication {
-        projectDir = self;
-        overrides = poetry2nix.overrides.withDefaults (final: super:
-          lib.mapAttrs
-            (attr: systems: super.${attr}.overridePythonAttrs
-              (old: {
-                nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: final.${a}) systems;
-              }))
-            {
-              # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
-              # package = [ "setuptools" ];
-            }
-        );
-      };
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          poetry2nix.overlays.default
-          (final: _: {
-            myapp = final.callPackage myapp { };
-          })
-        ];
-      };
+      
     in
     {
     darwinConfigurations = {
