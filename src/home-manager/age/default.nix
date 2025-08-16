c3b2e8334e7345248519260a1b0cc64ca833067e
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-16 17:48:08 +0700

    update

--- src/home-manager/age/default.nix
+++ src/home-manager/age/default.nix
@@ -3,7 +3,7 @@
   age = {
     secrets = {
       "github_token".file = ./secrets/github_token.age;
-      "tailscale_key".file = ./secrets/taiscale_key.age;
+      "tailscale_key".file = ./secrets/tailscale_key.age;
     };
     identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
   };
