14b16ba41925a8870b3740893bb0392c9b09c88c
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-16 17:16:19 +0700

    update

---
+++ src/home-manager/age/secrets/secrets.nix
@@ -0,0 +1,9 @@
+let
+  users = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICtklD5ou04FnuluU8mT+YhryqPzOq/p/Zds3DQQ+IN2 macmini"  
+    # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDDEXvxIw6DckDXhbt650gz0sthGm8xyt+PGfJ5OUA3x nixos"
+  ];
+in
+{
+  "github_token.age".publicKeys = users;
+}
