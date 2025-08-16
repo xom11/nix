bbeb6fabf614cb2b9728e1a89c4e44e9da5a7494
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-16 18:00:26 +0700

    update

--- src/home-manager/age/default.nix
+++ src/home-manager/age/default.nix
@@ -4,11 +4,13 @@
     secrets = {
       "github_token".file = ./secrets/github_token.age;
       "tailscale_key".file = ./secrets/tailscale_key.age;
+      "openai_key".file = ./secrets/openai_key.age;
     };
     identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
   };
   home.sessionVariables = {
     GITHUB_TOKEN = "$(cat ${config.age.secrets.github_token.path})";
     TAILSCALE_KEY = "$(cat ${config.age.secrets.tailscale_key.path})"; 
+    OPENAI_KEY = "$(cat ${config.age.secrets.openai_key.path})";
   };
 }
