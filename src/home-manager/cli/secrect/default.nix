bffa0f5ae422900d3ea9e168cef73d6c158b66d4
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-16 15:55:01 +0700

    update

--- src/home-manager/cli/secrect/default.nix
+++ src/home-manager/cli/secrect/default.nix
@@ -1,9 +1,16 @@
-{...}:
-# let
+{config, ...}:
 #   user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICtklD5ou04FnuluU8mT+YhryqPzOq/p/Zds3DQQ+IN2 macmini";
 
 # in
 {
-  # "secret1.age".publicKeys = [ user1 ];
-  age.secrets.secret1.file = ./secret1.age;
+  age = {
+    secrets = {
+      "secret1" = {
+        file = ./secret1.age;
+      };
+    };
+    identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+    # secretsDir = "/home/username/.local/share/agenix/agenix";
+    # secretsMountPoint = "/home/username/.local/share/agenix/agenix.d";
+  };
 }
