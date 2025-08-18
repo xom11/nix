603e3b883b9d601146a544cbd75b1bacaf642305
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-18 19:54:54 +0700

    update

--- src/home-manager/gui/dotfiles/sway/run-or-raise.sh
+++ src/home-manager/gui/dotfiles/sway/run-or-raise.sh
@@ -1,11 +1,14 @@
-
-# pidof kitty && swaymsg [app_id=^kitty$] focus || (swaymsg workspace "kitty" && kitty)
-
 if pidof kitty; then
-    # Nếu kitty đang chạy, chuyển trọng tâm (focus) đến cửa sổ đó
-    swaymsg '[app_id="kitty"] focus'
+    focused_id=$(swaymsg -t get_tree | jq '.. | select(.focused? and .focused == true).app_id' | tr -d '"')
+    echo "Focused app ID: $focused_id"
+    if [ "$focused_id" == "kitty" ]; then
+        echo "Kitty is already focused"
+        # swaymsg '[app_id="kitty"] focus'
+    else
+        echo "Focusing Kitty"
+        # swaymsg '[app_id="kitty"] focus'
+    fi
 else
-    # Nếu kitty chưa chạy, mở một workspace mới có tên "kitty" và khởi động kitty
     swaymsg 'workspace "kitty"'
     kitty &
 fi
