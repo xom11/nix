45d1f8cb63f294e8c41dd6c2e5d2257329628770
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-19 19:04:54 +0700

    update

--- src/nixos/base/default.nix
+++ src/nixos/base/default.nix
@@ -28,6 +28,13 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration # Comment out this line if you use KDE Connect
+    kdepim-runtime # Unneeded if you use Thunderbird, etc.
+    konsole # Comment out this line if you use KDE's default terminal app
+    oxygen
+    kwallet
+  ];
   # Sway
   programs.sway = {
     enable = true;
