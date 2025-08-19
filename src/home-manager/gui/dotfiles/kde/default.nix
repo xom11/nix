6f7cf48ad81e2190cea68d9e8804168d30e2b421
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-19 19:25:21 +0700

    update

--- src/home-manager/gui/dotfiles/kde/default.nix
+++ src/home-manager/gui/dotfiles/kde/default.nix
@@ -1,5 +1,10 @@
-{config, dotfileDir, lib, distro, ...}:
+{config, dotfileDir, lib, distro, pkgs, ...}:
 lib.mkIf (distro == "nixos") {
+  home.packages = with pkgs; [
+    # Add any KDE-specific packages here if needed
+    kdotool
+
+  ];
   home.file = {
     ".config/kglobalshortcutsrc" = {
       source = config.lib.file.mkOutOfStoreSymlink "${dotfileDir}/kde/kglobalshortcutsrc";
