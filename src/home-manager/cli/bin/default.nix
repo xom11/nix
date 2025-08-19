e219c4c4a12b128d43a6416d4dbf5be3f203ded5
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-19 18:24:41 +0700

    update

--- src/home-manager/cli/bin/default.nix
+++ src/home-manager/cli/bin/default.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 let
-  scriptsPath = ./scripts;
-  scriptFiles = builtins.readDir scriptsPath;
+  # Lọc các file để chỉ lấy script, bỏ qua chính file default.nix
+  scripts = builtins.filter (name: name != "default.nix") (builtins.attrNames (builtins.readDir ./.) );
 in
 {
   home.packages = builtins.map (name:
-    pkgs.writeShellScriptBin name (builtins.readFile (scriptsPath + "/${name}"))
-  ) (builtins.attrNames scriptFiles);
+    pkgs.writeShellScriptBin name (builtins.readFile (./. + "/${name}"))
+  ) scripts;
 }
