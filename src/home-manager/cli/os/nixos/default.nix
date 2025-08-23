680cd3cd963225710d62dfb82d3f6cd21accd778
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-23 16:22:48 +0700

    update

--- src/home-manager/cli/os/nixos/default.nix
+++ src/home-manager/cli/os/nixos/default.nix
@@ -6,6 +6,10 @@
       gcc
       caligula
       stdenv.cc.cc.lib
-      openvscode-server
   ];
+  imports = [
+    "${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
+  ];
+
+  services.vscode-server.enable = true;
 }
