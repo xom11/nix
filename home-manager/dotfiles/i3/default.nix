59dd3a76b9d9a083f80c0ab7ec3e6a1082f6b583
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-08-28 22:12:21 +0700

    update

--- home-manager/dotfiles/i3/default.nix
+++ home-manager/dotfiles/i3/default.nix
@@ -1,10 +1,10 @@
-{lib, pkgs, config, dotfileDir, ... }:
+{lib, pkgs, config, dotfileDir, device, ... }:
 lib.mkIf pkgs.stdenv.isLinux {
   home.file = {
     ".config/i3/config" = {
       source = config.lib.file.mkOutOfStoreSymlink "${dotfileDir}/i3/config";
     };
-    ".Xresources" = {
+    ".Xresources" = lib.mkIf (device == "desktop") {
       source = config.lib.file.mkOutOfStoreSymlink "${dotfileDir}/i3/Xresources";
     };
   };
