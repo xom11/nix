43a1df7358efb8165fe4f821d86935fc866c9644
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-29 19:45:25 +0700

    update

--- home-manager/environment/i18n/default.nix
+++ home-manager/environment/i18n/default.nix
@@ -1,28 +1,34 @@
-{pkgs, lib, device, ...}:
-lib.mkIf (device == "x1g6"|| device == "desktop") 
+{pkgs, lib, device, config, options, ...}:
+# lib.mkIf (device == "x1g6"|| device == "desktop") 
+let 
+  cfg = config.modules.i18n;
+in
 {
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5 = {
-      addons = with pkgs; [
-        # fcitx5-bamboo
-        fcitx5-gtk
-        fcitx5-unikey
-      ];
-      waylandFrontend = true; 
-      settings.inputMethod = {
-        GroupOrder."0" = "Default";
-        "Groups/0" = {
-          Name = "Default";
-          "Default Layout" = "us";
-          DefaultIM = "bamboo";
+  options.modules.i18n ={
+    enable = lib.mkEnableOption "Enable i18n settings";
+  };
+  config = lib.mkIf cfg.enable {
+    i18n.inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5 = {
+        addons = with pkgs; [
+          # fcitx5-bamboo
+          fcitx5-gtk
+          fcitx5-unikey
+        ];
+        waylandFrontend = true; 
+        settings.inputMethod = {
+          GroupOrder."0" = "Default";
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "bamboo";
+          };
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "unikey";
         };
-        "Groups/0/Items/0".Name = "keyboard-us";
-        "Groups/0/Items/1".Name = "unikey";
       };
     };
   };
-  home.packages = with pkgs; [
-  ];
 }
