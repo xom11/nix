0d0bab358fe6488a3a4629d7479cf38594540ef1
khanhkhanhlele <namkhanh2172@gmail.com>
2025-08-30 17:40:30 +0700

    update

--- home-manager/environment/x11/default.nix
+++ home-manager/environment/x11/default.nix
@@ -5,6 +5,11 @@ in
 {
   options.modules.x11 ={
     enable = lib.mkEnableOption "Enable x11 settings";
+    screen.dpi = lib.mkOption {
+      type = lib.types.int;
+      default = 144;
+      description = "Set screen DPI";
+    };
   };
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
@@ -20,5 +25,12 @@ in
       maim
     ];
     services.picom.enable = true;
+    home.file = {
+      ".Xresources" = {
+        text = ''
+          Xft.dpi: ${toString cfg.screen.dpi}
+        '';
+      };
+    };
   };
 }
