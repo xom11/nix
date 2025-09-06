f393f5a5801637f2a28a08d1d572666db96f4ba0
khanhkhanhlele <namkhanh2172@gmail.com>
2025-09-06 13:10:54 +0700

    update

--- scripts/setup-nixos.sh
+++ scripts/setup-nixos.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/kln-os/nix.git /tmp/nix -q --depth 1 
 sudo nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode disko /tmp/nix/nixos/disko/default.nix
-nixos-generate-config --no-filesystems
-nixos-install --flake --impure  ./nix#kln
+sudo nixos-generate-config --no-filesystems
+sudo nixos-install --flake --impure  ./nix#kln
