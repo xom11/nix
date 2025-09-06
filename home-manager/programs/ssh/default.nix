2effed0859d42cb95b931da0f1add4f8bd2d938a
khanhkhanhlele <namkhanh2172@gmail.com>
2025-09-06 14:28:44 +0700

    update

--- home-manager/programs/ssh/default.nix
+++ home-manager/programs/ssh/default.nix
@@ -23,7 +23,7 @@ in
 
       genSshKeyGen = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         if [ ! -f ~/.ssh/id_ed25519 ]; then
-          $(which ssh-keygen) -t ed25519 -f ~/.ssh/id_ed25519 -N ""
+          ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
         fi
       '';
     };
