7db59e2a80e8e7169b4e67461d963affec78b8ca
khanhkhanhlele <namkhanh2172@gmail.com>
2025-09-09 12:51:00 +0700

    update

--- hosts/x1g6/configuration.nix
+++ hosts/x1g6/configuration.nix
@@ -1,10 +1,9 @@
-{nixos-hardware, ... }:
+{ nixos-hardware, ... }:
 {
   imports = [
+    nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
     ../../nixos
     ./disko.nix
     ./hardware.nix
-    nixos-hardware.nixosModules.lenovo-thinkpad-x1-6th-gen
   ];
-
 }
