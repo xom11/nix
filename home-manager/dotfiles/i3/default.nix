89f0974565119382072ff91aeccdb22f28a0bbda
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-10-07 18:04:46 +0700

    add i3 check battery

--- home-manager/dotfiles/i3/default.nix
+++ home-manager/dotfiles/i3/default.nix
@@ -1,6 +1,7 @@
-{lib, config, dotfileDir, ... }:
+{lib, config, dotfileDir, pkgs, ... }:
 let
   cfg = config.modules.dotfiles.i3;
+  scripts = builtins.filter (name: name != "default.nix") (builtins.attrNames (builtins.readDir ./scripts) );
 in
 {
   options.modules.dotfiles.i3 = {
@@ -11,6 +12,9 @@ in
       ".config/i3/config" = {
         source = config.lib.file.mkOutOfStoreSymlink "${dotfileDir}/i3/config";
       };
+    home.packages = builtins.map (name:
+      pkgs.writeShellScriptBin name (builtins.readFile (./. + "/${name}"))
+    ) scripts;
     };
   };
 }
