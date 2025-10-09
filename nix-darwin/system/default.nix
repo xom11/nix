7e7b7a0b5c7e4641444b5dd8380fa2d6407c5d54
khanhkhanhlele <namkhanh2172@gmail.com>
2025-10-09 16:08:09 +0700

    update

--- nix-darwin/system/default.nix
+++ nix-darwin/system/default.nix
@@ -58,8 +58,19 @@
         NSGlobalDomain = {
           AppleLanguages = ["en"];
         };
+        "com.apple.symbolichotkeys" = {
+          AppleSymbolicHotKeys = {
+            # Disable 'Cmd + Space' for Spotlight Search
+            "64" = {
+              enabled = false;
+            };
+            # Disable 'Cmd + Alt + Space' for Finder search window
+            "65" = {
+              enabled = false;
+            };
+          };
+        };
       };
-
     };
   };
 }  
