399ccf9da43f60718e3166b5d1f63aed83794dfa
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-10-10 17:30:38 +0700

    karabine rule seperate file

--- home-manager/dotfiles/karabiner/default.nix
+++ home-manager/dotfiles/karabiner/default.nix
@@ -2,20 +2,27 @@
 {lib, config, dotfileDir, pkgs, ...}:
 let
   cfg = config.modules.dotfiles.karabiner;
+  rules_dir = "${dotfileDir}/karabiner/rules";
 in
 {
   options.modules.dotfiles.karabiner = {
     enable = lib.mkEnableOption "Enable karabiner dotfiles";
   };
   config = lib.mkIf cfg.enable {
-    # home.file = {
-    #   ".config/karabiner/karabiner.json" = {
-    #     source = config.lib.file.mkOutOfStoreSymlink "${dotfileDir}/karabiner/karabiner.json";
-    #   };
     home.activation = {
       karabiner = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         mkdir -p $HOME/.config/karabiner
         ${pkgs.yq-go}/bin/yq eval ${dotfileDir}/karabiner/karabiner.yml -o=json > $HOME/.config/karabiner/karabiner.json
+        ${pkgs.yq-go}/bin/yq eval '
+          .profiles[].complex_modifications.rules += 
+            load("'${rules_dir}'/modifier_keys.yml") +
+            load("'${rules_dir}'/tab_navigation.yml") +
+            load("'${rules_dir}'/homerow.yml") +
+            load("'${rules_dir}'/audio_brightness.yml") +
+            load("'${rules_dir}'/clipboard_number.yml") +
+            load("'${rules_dir}'/other.yml") +
+            []
+          ' ${dotfileDir}/karabiner/main.yml -o=json > $HOME/.config/karabiner/karabiner.json
         /opt/homebrew/bin/karabiner_cli --select-profile "Default profile"
       '';
     };
