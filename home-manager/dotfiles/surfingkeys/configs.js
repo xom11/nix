b9f2eaf422815980e2349d9b8ec866885f985811
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-10-16 16:29:32 +0700

    surfingeys add alias

--- home-manager/dotfiles/surfingkeys/configs.js
+++ home-manager/dotfiles/surfingkeys/configs.js
@@ -45,12 +45,14 @@ api.removeSearchAlias('s');
 api.removeSearchAlias('g');
 api.removeSearchAlias('e');
 api.removeSearchAlias('b');
+api.removeSearchAlias('y');
 unmap('on');
 api.addSearchAlias('gg', 'google', 'https://www.google.com/search?q=');
 api.addSearchAlias('tt', 'taostats', 'https://taostats.io/subnets/%s');
 api.addSearchAlias('gh', 'github', 'https://github.com/search?q=');
 api.addSearchAlias('np', 'nixpkgs', 'https://search.nixos.org/packages?channel=unstable&query=%s');
 api.addSearchAlias('no', 'mynixos', 'https://mynixos.com/search?q=%s');
+api.addSearchAlias('yt', 'youtube', 'https://www.youtube.com/results?search_query=%s');
 
 api.mapkey('ont', 'Open newtab', function () {
     api.tabOpenLink("www.google.com");
