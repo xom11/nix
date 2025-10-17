90c2ece0fe1802bd6ea61df505e65a1ede374c8c
khanhkhanhlele <namkhanh2172@gmail.com>
2025-10-17 12:11:47 +0700

    add syncthing

--- home-manager/services/default.nix
+++ home-manager/services/default.nix
@@ -1,15 +1,7 @@
 { pkgs, lib, device, ... }:
 
-lib.mkMerge [
-  {
-    # services.ssh-agent.enable = true;
-    # services.podman.enable = true;
-  }
-
-  (lib.mkIf (device == "x1g6" || device == "desktop") {
-  })
-
-  # (lib.mkIf (device == "x1g6") {
-  #   services.picom.enable = true;
-  # })
-]
+{
+  services.syncthing = {
+    enable = true;
+  };
+}
