4d8ea921097e6f76e2a1893e62f836699b84649c
khanhkhanhlele <namkhanh2172@gmail.com>
2025-10-23 18:14:57 +0700

    update

---
+++ hosts/vmware/README.md
@@ -0,0 +1,3 @@
+```
+curl -L https://raw.githubusercontent.com/kln-os/nix/refs/heads/main/hosts/vmware/install.sh | sh
+```
--- hosts/vmware/install.sh
+++ hosts/vmware/install.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/kln-os/nix.git /tmp/nix -q --depth 1 
-sudo nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode disko /tmp/nix/host/x1g6/disko.nix
-sudo nixos-install --impure --flake /tmp/nix#x1g6
+sudo nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode disko /tmp/nix/host/vmware/disko.nix
+sudo nixos-install --impure --flake /tmp/nix#vmware
 
