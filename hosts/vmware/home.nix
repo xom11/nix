c0c08d97299d7bfc4f19d7491928aea565be30ad
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-10-23 23:22:06 +0700

    update

--- hosts/vmware/home.nix
+++ hosts/vmware/home.nix
@@ -9,38 +9,38 @@
       sudo nixos-rebuild switch --impure --refresh --flake ~/.nix#x1g6
     '';
   };
-  # modules = {
-  #   i18n.enable = true;
-  #   fonts.enable = true;
-  #   x11.enable = true;
-  #   dotfiles = {
-  #     btop.enable = true;
-  #     i3.enable = true;
-  #     kitty.enable = true;
-  #     qutebrowser.enable = true;
-  #     vscode.enable = true;
-  #     rofi.enable = true;
-  #     ssh.enable = true;
-  #     yazi.enable = true;
-  #   };
-  #   pkgs = {
-  #     cli.enable = true;
-  #     gui.enable = true;
-  #     dev.enable = true;
-  #   };
-  #   programs = {
-  #     bin.enable = true;
-  #     nvim.enable = true;
-  #     git.enable = true;
-  #     tmux.enable = true;
-  #     zsh.enable = true;
-  #     ssh.enable = true;
-  #   };
-  #   secrets.enable = true;
-  #   sources = {
-  #     raiseorlaunch.enable = true;
-  #   };
-  # };
+  modules = {
+    i18n.enable = true;
+    fonts.enable = true;
+    x11.enable = true;
+    dotfiles = {
+      btop.enable = true;
+      i3.enable = true;
+      kitty.enable = true;
+      qutebrowser.enable = true;
+      vscode.enable = true;
+      rofi.enable = true;
+      ssh.enable = true;
+      yazi.enable = true;
+    };
+    pkgs = {
+      cli.enable = true;
+      # gui.enable = true;
+      dev.enable = true;
+    };
+    programs = {
+      bin.enable = true;
+      # nvim.enable = true;
+      git.enable = true;
+      tmux.enable = true;
+      zsh.enable = true;
+      ssh.enable = true;
+    };
+    secrets.enable = true;
+    sources = {
+      raiseorlaunch.enable = true;
+    };
+  };
   home.packages = [
   ];
 }
--- hosts/vmware/install.sh
+++ hosts/vmware/install.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/kln-os/nix.git /tmp/nix -q --depth 1 
-sudo nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode disko /tmp/nix/host/vmware/disko.nix
+sudo nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode disko /tmp/nix/hosts/vmware/disko.nix
 sudo nixos-install --impure --flake /tmp/nix#vmware
 
