92d7efa23d84d91b5629662229d1d65d3cece01b
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-10-28 00:32:19 +0700

    update

--- home-manager/dotfiles/hammerspoon/init.lua
+++ home-manager/dotfiles/hammerspoon/init.lua
@@ -1,6 +1,13 @@
 -- Enable Hammerspoon CLI
 hs.ipc.cliInstall()
 
+local install = hs.loadSpoon("SpoonInstall")
+
+local mySpoons = {
+    "TextClipboardHistory", 
+}
+
+
 -- Reload config with Cmd+Alt+Ctrl+Shift+R
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "R", function()
   hs.reload()
@@ -15,9 +22,23 @@ reverse_trackpad_scroll = hs.eventtap.new({hs.eventtap.event.types.scrollWheel},
         return false -- mouse: pass the event along
     end
     
-    event:setProperty(hs.eventtap.event.properties.scrollWheelEventDeltaAxis1,
-        -event:getProperty(hs.eventtap.event.properties.scrollWheelEventDeltaAxis1))
-    return false -- pass the event along
+    -- Đảo chiều cuộn dọc (theo dòng)
+    -- event:setProperty(hs.eventtap.event.properties.scrollWheelEventDeltaAxis1,
+    --     -event:getProperty(hs.eventtap.event.properties.scrollWheelEventDeltaAxis1))
+        
+    -- -- Đảo chiều cuộn ngang (theo dòng)
+    -- event:setProperty(hs.eventtap.event.properties.scrollWheelEventDeltaAxis2,
+    --     -event:getProperty(hs.eventtap.event.properties.scrollWheelEventDeltaAxis2))
+
+    -- Đảo chiều cuộn dọc (theo pixel) - QUAN TRỌNG
+    event:setProperty(hs.eventtap.event.properties.scrollWheelEventPointDeltaAxis1,
+        -event:getProperty(hs.eventtap.event.properties.scrollWheelEventPointDeltaAxis1))
+
+    -- Đảo chiều cuộn ngang (theo pixel) - QUAN TRỌNG
+    event:setProperty(hs.eventtap.event.properties.scrollWheelEventPointDeltaAxis2,
+        -event:getProperty(hs.eventtap.event.properties.scrollWheelEventPointDeltaAxis2))
+        
+    return false -- pass the event along (gửi đi sự kiện đã bị sửa)
 end):start()
 
 
