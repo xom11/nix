8f672be0e4aedd0a7623217461b4423b0b31541f
khanhkhanhlele <namkhanh2172@gmail.com>
2025-10-30 18:06:53 +0700

    update

--- hosts/vmware/configuration.nix
+++ hosts/vmware/configuration.nix
@@ -12,11 +12,7 @@
     };
   };
   # Bug: vmware DNS
-  # Disable NetworkManager's internal DNS resolution
-  networking.networkmanager.dns = "none";
 
-  # Configure DNS servers manually (this example uses Cloudflare and Google DNS)
-  # IPv6 DNS servers can be used here as well.
   networking.nameservers = [
     "1.1.1.1"
     "1.0.0.1"
