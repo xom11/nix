597210b1d0690206cc2bcdbc59a7934b041fc6e7
khanhkhanhlele <namkhanh2172@gmail.com>
2025-10-30 18:03:24 +0700

    update

--- hosts/vmware/configuration.nix
+++ hosts/vmware/configuration.nix
@@ -12,8 +12,19 @@
     };
   };
   # Bug: vmware DNS
+  # Disable NetworkManager's internal DNS resolution
+  networking.networkmanager.dns = "none";
+
+  # These options are unnecessary when managing DNS ourselves
+  networking.useDHCP = false;
+  networking.dhcpcd.enable = false;
+
+  # Configure DNS servers manually (this example uses Cloudflare and Google DNS)
+  # IPv6 DNS servers can be used here as well.
   networking.nameservers = [
-    "8.8.8.8"    # Google Public DNS
-    "1.1.1.1"    # Cloudflare DNS
+    "1.1.1.1"
+    "1.0.0.1"
+    "8.8.8.8"
+    "8.8.4.4"
   ];
 }
