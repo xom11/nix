513bd788186981427361bc52843af617e3ac2829
khanhkhanhlele <namkhanh2172@gmail.com>
2025-10-30 17:34:10 +0700

    update

--- hosts/vmware/configuration.nix
+++ hosts/vmware/configuration.nix
@@ -11,4 +11,9 @@
       desktop-environment.enable = true;
     };
   };
+  # Bug: vmware DNS
+  networking.nameservers = [
+    "8.8.8.8"    # Google Public DNS
+    "1.1.1.1"    # Cloudflare DNS
+  ];
 }
