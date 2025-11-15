5443b530223d3780d03224f7cfe22f77962e54c0
khanhkhanhlele <namkhanh20xx@gmail.com>
2025-11-15 18:17:05 +0700

    update

---
+++ home-manager/dotfiles/hammerspoon/MySpoons/LaunchTerminal.spoon/init.lua
@@ -0,0 +1,79 @@
+local obj = {}
+obj.__index = obj
+
+local hyper = { "cmd", "ctrl", "alt" }
+
+local defaultShortcuts = {
+	{ "space", "kitty" },
+	{ "b", "Brave Browser" },
+	{ "d", "Discord" },
+	{ "f", "Finder" },
+	{ "k", "Google Keep" },
+	{ "g", "Google Gemini" },
+	{ "m", "Messenger" },
+	{ "n", "Notion" },
+	{ "t", "Telegram" },
+	{ "s", "System Settings" },
+	{ "v", "Visual Studio Code" },
+	{ "y", "Youtube" },
+	{ "z", "Zalo" },
+}
+
+local rb = hs.loadSpoon("RecursiveBinder")
+
+local function moveCursorToCenter()
+	local focusedApp = hs.application.frontmostApplication()
+
+	if focusedApp then
+		local focusedWindow = focusedApp:focusedWindow()
+
+		if focusedWindow then
+			local frame = focusedWindow:frame()
+
+			local centerX = frame.x + frame.w / 2
+			local centerY = frame.y + frame.h / 2
+
+			hs.mouse.setAbsolutePosition({ x = centerX, y = centerY })
+		end
+	end
+end
+
+local function launch(appName)
+	-- Error when using window instead of application: pwa
+	local focusedApp = hs.application.frontmostApplication()
+	local _, focusedBundleID = hs.osascript.applescript([[id of app (path to frontmost application as text)]])
+	local _, targetBundleID = hs.osascript.applescript([[id of app "]] .. appName .. [["]])
+
+	if not targetBundleID then
+		hs.alert.show("Application '" .. appName .. "' not found!")
+		return
+	end
+
+	-- print("Focused Bundle ID: " .. tostring(focusedBundleID))
+	-- print("Target Bundle ID: " .. tostring(targetBundleID))
+	if focusedBundleID == targetBundleID then
+		focusedApp:hide()
+	else
+		hs.application.launchOrFocusByBundleID(targetBundleID)
+		moveCursorToCenter()
+	end
+end
+
+function obj:init()
+
+	-- Build a keymap for shortcuts so that hyper + x + <key> launches the app
+	local keymap = {}
+	for _, shortcut in ipairs(defaultShortcuts) do
+		local key = shortcut[1]
+		local appName = shortcut[2]
+		-- rb.singleKey will add 'shift' modifier automatically for uppercase letters
+		keymap[rb.singleKey(key, appName)] = function()
+			launch(appName)
+		end
+	end
+
+	local starter = rb.recursiveBind(keymap)
+	hs.hotkey.bind(hyper, "x", starter)
+end
+
+return obj
