196c8ed361cef7be3a4a4b9346a7d418a8101c25
khanhkhanhlele <namkhanh2172@gmail.com>
2025-11-26 14:22:25 +0700

    update

--- home-manager/programs/nvim/telescope.nix
+++ home-manager/programs/nvim/telescope.nix
@@ -1,6 +1,7 @@
 {
   config,
   ckModule,
+  pkgs,
   ...
 }:
 ckModule config ./.
@@ -8,20 +9,22 @@ ckModule config ./.
   programs.nixvim.plugins.telescope = {
     enable = true;
 
-    extensions.ui-select.enable = true;
-    extensions.frecency = {
-      enable = true;
-      # FIX: issue https://github.com/nvim-telescope/telescope-frecency.nvim/issues/270
-      settings.db_safe_mode = false;
-      # Fix: issue https://github.com/nvim-telescope/telescope-frecency.nvim/issues/105
-      settings.db_validate_threshold = 1;
-    };
-    extensions.fzf-native.enable = true;
-    extensions.file-browser = {
-      enable = true;
-      settings.hidden = true;
-      settings.depth = 9999999999;
-      settings.auto_depth = true;
+    extensions = {
+      ui-select.enable = true;
+      frecency = {
+        enable = true;
+        # FIX: issue https://github.com/nvim-telescope/telescope-frecency.nvim/issues/270
+        settings.db_safe_mode = false;
+        # Fix: issue https://github.com/nvim-telescope/telescope-frecency.nvim/issues/105
+        settings.db_validate_threshold = 1;
+      };
+      fzf-native.enable = true;
+      file-browser = {
+        enable = true;
+        settings.hidden = true;
+        settings.depth = 9999999999;
+        settings.auto_depth = true;
+      };
     };
     keymaps = {
       "<leader>ff" = "find_files";
@@ -32,15 +35,28 @@ ckModule config ./.
       "<A-f>" = "live_grep";
       "<leader>fo" = "oldfiles";
     };
-    settings.defaults = {
-      file_ignore_patterns = [
-        "^.git/"
-        "^.mypy_cache/"
-        "^__pycache__/"
-        "^output/"
-        "^data/"
-        "%.ipynb"
-      ];
+    settings = {
+      defaults = {
+        vimgrep_arguments = ["${pkgs.ripgrep}/bin/rg" "-L" "--color=never" "--no-heading" "--with-filename" "--line-number" "--column" "--smart-case" "--fixed-strings"];
+        selection_caret = "  ";
+        entry_prefix = "  ";
+        layout_strategy = "flex";
+        layout_config = {
+          horizontal = {
+            prompt_position = "bottom";
+          };
+        };
+        sorting_strategy = "ascending";
+        set_env.COLORTERM = "truecolor";
+        file_ignore_patterns = [
+          "^.git/"
+          "^.mypy_cache/"
+          "^__pycache__/"
+          "^output/"
+          "^data/"
+          "%.ipynb"
+        ];
+      };
     };
   };
   programs.nixvim.keymaps = [
@@ -48,5 +64,8 @@ ckModule config ./.
       key = "<leader><leader>";
       action = "<cmd>Telescope frecency workspace=CWD<cr>";
     }
+  ];
+  home.packages = with pkgs; [
+    ripgrep
   ];
 }
