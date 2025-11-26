564a24a2db465d9b8433a7ad62d7fab15a1c0be1
khanhkhanhlele <namkhanh2172@gmail.com>
2025-11-26 14:37:28 +0700

    update

--- home-manager/programs/nvim/conform.nix
+++ home-manager/programs/nvim/conform.nix
@@ -9,20 +9,28 @@ ckModule config ./.
   programs.nixvim.plugins.conform-nvim = {
     enable = true;
     settings = {
+      format_on_save = {
+        lspFallback = true;
+        timeoutMs = 500;
+      };
+      notify_on_error = true;
       formatters_by_ft = {
-        javascript = ["prettierd"];
-        typescript = ["prettierd"];
-        yaml = ["prettierd"];
+        html = [["prettierd" "prettier"]];
+        css = [["prettierd" "prettier"]];
+        scss = ["prettierd" "prettier"];
+        javascript = [["prettierd" "prettier"]];
+        javascriptreact = [["prettierd" "prettier"]];
+        typescript = [["prettierd" "prettier"]];
+        typescriptreact = [["prettierd" "prettier"]];
+        markdown = [["prettierd" "prettier"]];
+        yaml = [["yamllint" "yamlfmt"]];
         json = ["prettierd"];
-        markdown = ["prettierd"];
-        scss = ["prettierd"];
-        css = ["prettierd"];
         nix = ["alejandra"];
-
         lua = ["stylua"];
         python = ["black"];
         rust = ["rustfmt"];
         sh = ["shfmt"];
+        bash = ["shfmt"];
         "_" = ["trim_whitespace"];
       };
     };
@@ -43,5 +51,7 @@ ckModule config ./.
     stylua
     alejandra
     prettierd
+    yamllint
+    yamlfmt
   ];
 }
--- home-manager/programs/nvim/treesitter.nix
+++ home-manager/programs/nvim/treesitter.nix
@@ -6,23 +6,27 @@
 }:
 ckModule config ./.
 {
-  programs.nixvim.plugins = {
-    treesitter = {
-      enable = true;
-      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-        bash
-        json
-        lua
-        markdown
-        nix
-        toml
-        yaml
-        python
-      ];
-      settings = {
-        auto_install = true;
-        highlight.enable = true;
-        indent.enable = true;
+  programs.nixvim = {
+    filetype.extension.kbd = "lisp";
+    plugins = {
+      treesitter = {
+        enable = true;
+        grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+          bash
+          json
+          lua
+          markdown
+          nix
+          toml
+          yaml
+          python
+          commonlisp
+        ];
+        settings = {
+          auto_install = true;
+          highlight.enable = true;
+          indent.enable = true;
+        };
       };
     };
   };
