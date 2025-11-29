0dc5a2f16ab6719ac54aee3d9e51da9fefaa441d
khanhkhanhlele <namkhanh2172@gmail.com>
2025-11-29 16:27:00 +0700

    update

--- home-manager/programs/nvim/motion.nix
+++ home-manager/programs/nvim/motion.nix
@@ -17,18 +17,17 @@ ckModule config ./.
     };
 
     # https://nix-community.github.io/nixvim/plugins/flash/index.html#flash
+    # https://github.com/folke/flash.nvim/
     flash = {
       enable = true;
       luaConfig.post = ''
         local map = vim.keymap.set
-        -- quick jumping
+        -- x visual, o operator (d, y, etc.), c command-line /?
+        -- treesitter : jumpping in code blocks
         map({'n', 'x', 'o'}, 's', function() require("flash").jump() end, { desc = "Flash" })
-        -- code block jumping
         map({'n', 'x', 'o'}, 'S', function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
-        -- remote jumping
         map({'o'}, 'r', function() require("flash").remote() end, { desc = "Remote Flash" })
         map({'o', 'x'}, 'R', function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
-        -- c: command-line /?
         map({'c'}, '<c-s>', function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
       '';
       settings = {
