6e134273ec4aaeb7718fbb38c474dd894dd16b05
khanhkhanhlele <namkhanh2172@gmail.com>
2025-12-20 14:30:29 +0700

    update

--- windows/dotfiles/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows/dotfiles/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -9,24 +9,33 @@ Import-Module ZLocation
 # Set-PSReadLineOption -PredictionSource History
 # Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-
-function v{nvim.exe}
-function lzg {lazygit}
-function lzd {lazydocker}
-function ff {fastfetch}
-
-function ga {git add $args}
-function gc {
-    if ($args.Count -eq 0) {
-        git commit
-    } else {
-        git commit -m $args
+$conflicts = @("gc", "gl", "gp")
+foreach ($alias in $conflicts) {
+    if (Test-Path "alias:$alias") {
+        Remove-Item "alias:$alias" -Force
     }
 }
-function gp {git push}
-function gst {git status}
-function gl {git pull}
+function v { nvim.exe }
+function lzg { lazygit }
+function lzd { lazydocker }
+function ff { fastfetch }
 
-function py {python $args}
+function ga { git add $args }
+function gc {
+    if ($args.Count -eq 0) {
+        Write-Warning "Commit message is required."
+    }
+    else {
+        $message = $args -join " "
+        git commit -m "$message"
+    }
+}
+function gp { git push $args }
+function gst { git status $args }
+function gl { git pull $args }
+function glog { 
+    git log --graph --oneline --decorate --all 
+}
+function py { python $args }
 # Set-Alias spy source .venv\bin\activate
-function m {micromamba.exe}
+function m { micromamba.exe }
