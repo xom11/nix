ffc113aaf8552a9af72bd9a239d44e3d3200a04e
khanhkhanhlele <namkhanh2172@gmail.com>
2025-12-25 04:26:47 +0000

    update

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -9,7 +9,7 @@ mkModule config ./. {
     settings = {
       user = {
         name = "khanhkhanhlele";
-        email = "khanhkhanhlele";
+        email = "namkhanh2172@gmail.com";
       };
       init.defaultBranch = "main";
       credential.helper = "store";
