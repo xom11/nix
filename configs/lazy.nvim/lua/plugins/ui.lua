d69094623be870e6f1e46738139931cc5f5c3a4e
khanhkhanhlele <namkhanh2172@gmail.com>
2026-01-12 12:10:26 +0700

    update

--- configs/lazy.nvim/lua/plugins/ui.lua
+++ configs/lazy.nvim/lua/plugins/ui.lua
@@ -66,15 +66,17 @@ return {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
 			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
 			"rcarriga/nvim-notify",
 		},
 	},
 
   {
     "rcarriga/nvim-notify",
-  };
+    opts = {
+      background_colour = "#000000",
+      -- timeout = 3000,
+    },
+  },
 
 	{
 		"xiyaowong/transparent.nvim",
