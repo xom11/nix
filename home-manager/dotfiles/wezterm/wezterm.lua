f487bb0c47240a4102fb633f1aed02f8f03c815d
khanhkhanhlele <namkhanh2172@gmail.com>
2026-01-12 05:16:31 +0000

    update

--- home-manager/dotfiles/wezterm/wezterm.lua
+++ home-manager/dotfiles/wezterm/wezterm.lua
@@ -1,50 +1,50 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local act = wezterm.action
 local mux = wezterm.mux
 
 -- pwsh shell in windows
 local function get_default_prog()
-  if wezterm.target_triple:find("windows") then
-    return { "pwsh.exe", "-NoLogo" }
-  end
-  return nil
+	if wezterm.target_triple:find("windows") then
+		return { "pwsh.exe", "-NoLogo" }
+	end
+	return nil
 end
 config.default_prog = get_default_prog()
 
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14.0
-config.color_scheme = 'Catppuccin Macchiato'
+config.color_scheme = "Catppuccin Macchiato"
 
 -- full screen startup
 wezterm.on("gui-startup", function()
-  local tab, pane, window = mux.spawn_window{}
-  window:gui_window():maximize()
+	local tab, pane, window = mux.spawn_window({})
+	window:gui_window():maximize()
 end)
 
 -- opacity
 config.window_background_opacity = 0.8
-config.win32_system_backdrop = 'Acrylic' -- 'Acrylic', 'Mica', 'Tabbed'
+config.win32_system_backdrop = "Acrylic" -- 'Acrylic', 'Mica', 'Tabbed'
 
 config.window_decorations = "RESIZE"
 
 config.keys = {
-  { key = '|', mods = 'CTRL|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-  { key = '-', mods = 'CTRL|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
-  { key = 'w', mods = 'CTRL|SHIFT', action = wezterm.action.CloseCurrentTab { confirm = true } },
-  { key = 'LeftArrow',  mods = 'CTRL|SHIFT', action = wezterm.action.ActivateTabRelative(-1) },
-  { key = 'RightArrow', mods = 'CTRL|SHIFT', action = wezterm.action.ActivateTabRelative(1) },
-    {
-    key = 'n',
-    mods = 'SHIFT|CTRL',
-    action = wezterm.action.ToggleFullScreen,
-  },
+	{ key = "|", mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "-", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "w", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
+	{ key = "LeftArrow", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "RightArrow", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
 }
 config.enable_scroll_bar = false
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 return config
