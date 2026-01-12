4183b9de80c961e22a99ffe9c15f066d8d8a49ee
khanhkhanhlele <namkhanh2172@gmail.com>
2026-01-12 20:15:23 +0700

    update

--- home-manager/programs/nixvim/plugins/ui.nix
+++ home-manager/programs/nixvim/plugins/ui.nix
@@ -13,52 +13,57 @@ ckModule config ./..
     noice.enable = true;
     image.enable = true;
     barbecue.enable = true;
-    # notify.enable = true; 
-  };
-  programs.nixvim.plugins.transparent = {
-    enable = true;
-    luaConfig.post = ''
-      require('transparent').clear_prefix('NeoTree')
-      require('transparent').clear_prefix('Telescope')
-
-      vim.cmd("highlight Normal guibg=NONE")
-      vim.cmd("highlight Lualine guibg=NONE")
-      vim.cmd("highlight Lualine guifg=NONE")
-      vim.cmd("highlight NormalNC guibg=NONE")
-      -- Highlight for cursor line
-      -- vim.cmd("highlight CursorLine guibg=NONE")
-
-    '';
-    settings = {
+    notify = {
       enable = true;
-      # table: groups you don't want to clear
-      exclude_groups = [
-        "CursorLine"
-      ];
-      # table: additional groups that should be cleared
-      extra_groups = [
-        "NeoTreeNormal"
-        "NeoTreeNormalNC"
-        "NeoTreeFloat"
-        "NeoTreeFloatBorder"
+      settings = {
+        background_colour = "#000000";
+      };
+    };
+    transparent = {
+      enable = true;
+      luaConfig.post = ''
+        require('transparent').clear_prefix('NeoTree')
+        require('transparent').clear_prefix('Telescope')
 
-        "TelescopeNormal"
-        "TelescopeBorder"
-        "TelescopePromptNormal"
-        "TelescopePromptBorder"
-        "TelescopeResultsNormal"
-        "TelescopePreviewNormal"
+        vim.cmd("highlight Normal guibg=NONE")
+        vim.cmd("highlight Lualine guibg=NONE")
+        vim.cmd("highlight Lualine guifg=NONE")
+        vim.cmd("highlight NormalNC guibg=NONE")
+        -- Highlight for cursor line
+        -- vim.cmd("highlight CursorLine guibg=NONE")
 
-        "LualineNormal"
-        "LualineNC"
+      '';
+      settings = {
+        enable = true;
+        # table: groups you don't want to clear
+        exclude_groups = [
+          "CursorLine"
+        ];
+        # table: additional groups that should be cleared
+        extra_groups = [
+          "NeoTreeNormal"
+          "NeoTreeNormalNC"
+          "NeoTreeFloat"
+          "NeoTreeFloatBorder"
 
-        "FzfLuaBorder"
-        "FzfLuaNormal"
-        "FzfLuaTitle"
-        "FzfLuaPreviewBorder"
-        "FzfLuaPreviewNormal"
-        "FzfLuaPreviewTitle"
-      ];
+          "TelescopeNormal"
+          "TelescopeBorder"
+          "TelescopePromptNormal"
+          "TelescopePromptBorder"
+          "TelescopeResultsNormal"
+          "TelescopePreviewNormal"
+
+          "LualineNormal"
+          "LualineNC"
+
+          "FzfLuaBorder"
+          "FzfLuaNormal"
+          "FzfLuaTitle"
+          "FzfLuaPreviewBorder"
+          "FzfLuaPreviewNormal"
+          "FzfLuaPreviewTitle"
+        ];
+      };
     };
   };
 }
