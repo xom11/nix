aadab174a74b4c0cb9f8aaa6fb8ee3f46ed84204
khanhkhanhlele <namkhanh2172@gmail.com>
2026-01-13 11:55:21 +0700

    update

--- home-manager/programs/nixvim/plugins/neotree.nix
+++ home-manager/programs/nixvim/plugins/neotree.nix
@@ -20,6 +20,41 @@ ckModule config ./..
           "/" = "filter_on_submit";
           "h" = "show_help";
           "c" = "copy_to_clipboard";
+          "yy" = "copy_path";
+          "yf" = "copy_name";
+          "yr" = "copy_relative_path";
+        };
+      };
+      commands = {
+        copy_path = {
+          __raw = ''
+            function(state)
+              local p = state.tree:get_node().path
+              vim.fn.setreg("+", p)
+              vim.notify("Copied to clipboard: " .. p)
+            end
+          '';
+        };
+
+        copy_name = {
+          __raw = ''
+            function(state)
+              local n = state.tree:get_node().name
+              vim.fn.setreg("+", n)
+              vim.notify("Copied to clipboard: " .. n)
+            end
+          '';
+        };
+
+        copy_relative_path = {
+          __raw = ''
+            function(state)
+              local p = state.tree:get_node().path
+              local r = vim.fn.fnamemodify(p, ":.")
+              vim.fn.setreg("+", r)
+              vim.notify("Copied to clipboard: " .. r)
+            end
+          '';
         };
       };
     };
