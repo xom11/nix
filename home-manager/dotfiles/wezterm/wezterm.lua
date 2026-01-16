a4dd25948a95ce0033b4c4e297fc8ffec66aefa0
khanhkhanhlele <namkhanh2172@gmail.com>
2026-01-16 10:09:03 +0700

    update

--- home-manager/dotfiles/wezterm/wezterm.lua
+++ home-manager/dotfiles/wezterm/wezterm.lua
@@ -33,29 +33,13 @@ config.keys = {
 	{ key = "|", mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	{ key = "-", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
 	{ key = "w", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
-	{ key = "r", mods = "CTRL|SHIFT", action = wezterm.reload_configuration() },
+	-- { key = "r", mods = "CTRL|SHIFT", action = wezterm.reload_configuration() },
 	{ key = "LeftArrow", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
 	{ key = "RightArrow", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
 	{
 		key = "n",
 		mods = "SHIFT|CTRL",
 		action = wezterm.action.ToggleFullScreen,
-	},
-	{
-		key = "E",
-		mods = "CTRL|SHIFT",
-		action = act.PromptInputLine({
-			description = "Enter new name for tab",
-			initial_value = "My Tab Name",
-			action = wezterm.action_callback(function(window, pane, line)
-				-- line will be `nil` if they hit escape without entering anything
-				-- An empty string if they just hit enter
-				-- Or the actual line of text they wrote
-				if line then
-					window:active_tab():set_title(line)
-				end
-			end),
-		}),
 	},
 }
 config.enable_scroll_bar = false
