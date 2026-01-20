1aa3c28fe661e9c3e3dfd9f90839c7dbc9b69b97
khanhkhanhlele <namkhanh2172@gmail.com>
2026-01-20 18:43:38 +0700

    update

--- home-manager/programs/nixvim/default.nix
+++ home-manager/programs/nixvim/default.nix
@@ -48,19 +48,6 @@ in
         if vim.g.have_nerd_font then
           require('nvim-web-devicons').setup {}
         end
-
-
-        vim.g.clipboard = {
-          name = 'OSC 52',
-          copy = {
-            ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-            ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-          },
-          paste = {
-            ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-            ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-          },
-        }
       '';
 
       extraConfigLuaPost = ''
