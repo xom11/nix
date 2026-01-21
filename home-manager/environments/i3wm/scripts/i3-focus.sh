dc95209a1f36e7ea12d1b1e9f838dac83632a937
a <a>
2026-01-21 15:10:33 +0700

    update

--- home-manager/environments/i3wm/scripts/i3-focus.sh
+++ home-manager/environments/i3wm/scripts/i3-focus.sh
@@ -2,7 +2,6 @@
 
 INSTANCE=$1
 LAUNCH_CMD=$2
-USE_NEW_WS=${3:-true}
 
 FOCUSED_INSTANCE=$(i3-msg -t get_tree | jq -r '.. | select(.focused? == true).window_properties.instance')
 
@@ -11,9 +10,7 @@ if [ "$FOCUSED_INSTANCE" = "$INSTANCE" ]; then
   i3-msg [con_mark=_back] focus
 else
   if ! i3-msg "[instance=\"^$INSTANCE$\"] focus" >/dev/null 2>&1; then
-    if [ "$USE_NEW_WS" = "true" ]; then
-      i3-msg workspace "$INSTANCE"
-    fi
+    i3-msg workspace "$INSTANCE"
     exec $LAUNCH_CMD
   fi
 fi
