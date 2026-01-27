cc561914e1f6d128f15322dfec24393b4c081c73
khanhkhanhlele <namkhanh2172@gmail.com>
2026-01-28 00:06:38 +0700

    update

--- configs/lazy.nvim/lua/plugins/cmp.lua
+++ configs/lazy.nvim/lua/plugins/cmp.lua
@@ -1,8 +1,13 @@
 return {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
 		"hrsh7th/cmp-emoji",
+		"L3MON4D3/LuaSnip", -- Engine snippet
+		"saadparwaiz1/cmp_luasnip",
 	},
 	opts = function(_, opts)
 		local cmp = require("cmp")
@@ -88,5 +93,6 @@ return {
 				{ name = "cmdline" },
 			}),
 		})
+		return opts
 	end,
 }
