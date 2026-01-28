29318586fa8bebfabe92a395f6da32c6db67b35e
khanhkhanhlele <namkhanh2172@gmail.com>
2026-01-29 00:43:09 +0700

    update

--- home-manager/programs/nixvim/lua/clipboard.lua
+++ home-manager/programs/nixvim/lua/clipboard.lua
@@ -1,17 +1,5 @@
-if vim.env.SSH_TTY then
-	vim.g.clipboard = {
-		name = "OSC 52",
-		copy = {
-			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-		},
-		paste = {
-			["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-			["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-		},
-	}
-end
-if vim.env.TMUX ~= nil then
+-- SSH + TMUX
+if vim.env.SSH_TTY and vim.env.TMUX then
 	local copy = { "tmux", "load-buffer", "-w", "-" }
 	local paste = { "bash", "-c", "tmux refresh-client -l && sleep 0.05 && tmux save-buffer -" }
 	vim.g.clipboard = {
@@ -25,5 +13,18 @@ if vim.env.TMUX ~= nil then
 			["*"] = paste,
 		},
 		cache_enabled = 0,
+	}
+-- SSH + not TMUX
+elseif vim.env.SSH_TTY then
+	vim.g.clipboard = {
+		name = "OSC 52",
+		copy = {
+			["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+			["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+		},
+		paste = {
+			["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+			["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+		},
 	}
 end
