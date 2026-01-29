1d55d8de66fb5eecea07fee9a316d31d8966f63f
khanhkhanhlele <namkhanh2172@gmail.com>
2026-01-29 10:32:04 +0700

    update

--- home-manager/dotfiles/wezterm/wezterm.lua
+++ home-manager/dotfiles/wezterm/wezterm.lua
@@ -43,6 +43,20 @@ config.keys = {
 	{ key = "k", mods = mod, action = wezterm.action.ActivatePaneDirection("Up") },
 	{ key = "j", mods = mod, action = wezterm.action.ActivatePaneDirection("Down") },
 	{ key = "n", mods = mod, action = wezterm.action.ToggleFullScreen },
+  { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+	{
+		key = "c",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			local selection_text = window:get_selection_text_for_pane(pane)
+			local is_selection_active = string.len(selection_text) ~= 0
+			if is_selection_active then
+				window:perform_action(wezterm.action.CopyTo("ClipboardAndPrimarySelection"), pane)
+			else
+				window:perform_action(wezterm.action.SendKey({ key = "c", mods = "CTRL" }), pane)
+			end
+		end),
+	},
 }
 config.enable_scroll_bar = false
 config.window_padding = {
