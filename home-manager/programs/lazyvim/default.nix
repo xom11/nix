0ce14d0308fa755fd4d9a4fbcdca8c38e0fdb36f
khanhkhanhlele <namkhanh2172@gmail.com>
2026-02-11 23:16:49 +0700

    update

--- home-manager/programs/lazyvim/default.nix
+++ home-manager/programs/lazyvim/default.nix
@@ -8,8 +8,21 @@
 in
   mkModule config ./. {
     home.file = {
-      ".config/lazyvim" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${pwd}";
+      ".config/lazyvim/lua/config" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${pwd}/lua/config";
+      };
+      ".config/lazyvim/lua/plugins" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${pwd}/lua/plugins";
+      };
+      ".config/lazyvim/init.lua" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${pwd}/init.lua";
+      };
+      ".config/lazyvim/lazy-lock.json" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${pwd}/lazy-lock.json";
+      };
+      # share lua configs between nixvim and lazyvim
+      ".config/lazyvim/lua/nixvim" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${pwd}/../nixvim/lua";
       };
     };
   }
--- home-manager/programs/lazyvim/init.lua
+++ home-manager/programs/lazyvim/init.lua
@@ -1,3 +1,12 @@
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
+
+-- lua configs from nixvim
+local nixvim_dir = vim.fn.stdpath("config") .. "/lua/nixvim"
+
+for file in vim.fs.dir(nixvim_dir) do
+  if file:match("%.lua$") then
+    require("nixvim." .. file:gsub("%.lua$", ""))
+  end
+end
