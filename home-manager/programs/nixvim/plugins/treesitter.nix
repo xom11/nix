26ec7a5d140d34b07944b7071b85c18f120bcb15
khanhkhanhlele <namkhanh2172@gmail.com>
2026-02-11 22:31:35 +0700

    update

--- home-manager/programs/nixvim/plugins/treesitter.nix
+++ home-manager/programs/nixvim/plugins/treesitter.nix
@@ -9,29 +9,11 @@ ckModule config ./..
   programs.nixvim = {
     filetype.extension.kbd = "lisp";
     extraPackages = with pkgs; [
-      tree-sitter 
+      tree-sitter
     ];
     plugins = {
       treesitter = {
         enable = true;
-        # grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-        #   bash
-        #   json
-        #   lua
-        #   markdown
-        #   nix
-        #   toml
-        #   yaml
-        #   python
-        #   commonlisp
-        #   powershell
-        #   html
-        #   css
-        #   javascript
-        #   typescript
-        #   tsx
-        #   dockerfile
-        # ];
         grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
         settings = {
           auto_install = false;
