bea1105df48ebf723b5b100b89fc6bf02b5fb83e
a <a>
2026-02-23 17:00:00 +0700

    update

--- home-manager/environments/i3wm/scripts/set-dpi.sh
+++ home-manager/environments/i3wm/scripts/set-dpi.sh
@@ -6,7 +6,7 @@ MONITOR=$(xrandr | grep " connected primary" | awk '{print $1}')
 WIDTH=$(xrandr | grep "$MONITOR" | grep -oP '\d+x\d+' | cut -d'x' -f1)
 
 if [ "$WIDTH" -gt 2000 ]; then
-    DPI=144
+    DPI=120
 else
     DPI=192
 fi
