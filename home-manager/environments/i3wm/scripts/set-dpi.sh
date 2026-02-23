e104253b80a1c5e15635c9172e7f4d0d3a569053
a <a>
2026-02-23 16:13:33 +0700

    update

--- home-manager/environments/i3wm/scripts/set-dpi.sh
+++ home-manager/environments/i3wm/scripts/set-dpi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# dpi-scale: 96-100% 120-125% 144-150% 192-200%
 MONITOR=$(xrandr | grep " connected primary" | awk '{print $1}')
 
 WIDTH=$(xrandr | grep "$MONITOR" | grep -oP '\d+x\d+' | cut -d'x' -f1)
@@ -7,11 +8,8 @@ WIDTH=$(xrandr | grep "$MONITOR" | grep -oP '\d+x\d+' | cut -d'x' -f1)
 if [ "$WIDTH" -gt 2000 ]; then
     DPI=144
 else
-    DPI=120
+    DPI=192
 fi
 
 echo "Xft.dpi: $DPI" | xrdb -merge
 
-#  Cập nhật cho các ứng dụng GTK (như Files, Settings)
-SCALE=$(echo "scale=2; $DPI / 96" | bc)
-gsettings set org.gnome.desktop.interface text-scaling-factor $SCALE
