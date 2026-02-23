9fee97fcab3e118d94fbf0ac0cdf9c50dfba5dab
a <a>
2026-02-23 15:49:34 +0700

    update

--- home-manager/environments/i3wm/ubuntu/default.nix
+++ home-manager/environments/i3wm/ubuntu/default.nix
@@ -5,11 +5,6 @@
 }:
 mkModule config ./. {
   home.file = {
-    # 96, 120, 144, 192 -> scale 1x, 1.25x, 1.5x, 2x
-    # ".Xresources".text = ''
-    #     Xft.dpi: 144
-    #   '';
-
     # fcitx5
     ".xprofile".text =''
       export XMODIFIERS="@im=fcitx"
