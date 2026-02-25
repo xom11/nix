367f606de4c4c43d899f1c42bc066b7e10755801
khanhkhanhlele <namkhanh2172@gmail.com>
2026-02-25 17:35:07 +0700

    update

--- home-manager/programs/nixvim/lua/extras/language-nvim.lua
+++ home-manager/programs/nixvim/lua/extras/language-nvim.lua
@@ -1,96 +1,52 @@
 -- set language based on vim mode
--- requires macism https://github.com/laishulu/macism
--- recommend installing it by brew
+-- requires macism (macOS) or fcitx5-remote (Linux)
 
-vim = vim
 local sysname = vim.loop.os_uname().sysname
 local is_mac = sysname == "Darwin" and vim.fn.executable("macism") == 1
 local is_linux = sysname == "Linux" and vim.fn.executable("fcitx5-remote") == 1
 
--- BUG conflix with hammerspoon switch language
-if is_mac then
-    local english_layout = "com.apple.keylayout.ABC"
-    local last_insert_layout = english_layout
-
-    local function get_current_layout()
-        local f = io.popen("macism")
-        local layout = nil
-        if f ~= nil then
-            layout = f:read("*all"):gsub("\n", "")
-            f:close()
-        end
-        -- print(layout)
-        return layout
-    end
-
-    vim.api.nvim_create_autocmd("InsertLeave", {
-        callback = function()
-            last_insert_layout = get_current_layout()
-            os.execute("macism " .. english_layout)
-        end,
-    })
-
-    -- vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
-    -- 	callback = function()
-    -- 		os.execute("macism " .. english_layout)
-    -- 	end,
-    -- })
-
-    -- BUG hammerspoon
-    -- vim.api.nvim_create_autocmd("InsertEnter", {
-    --     callback = function()
-    --         os.execute("macism " .. last_insert_layout)
-    --     end,
-    -- })
-    --
-    -- vim.api.nvim_create_autocmd("FocusGained", {
-    --     callback = function()
-    --         if vim.fn.mode() == "i" then
-    --             os.execute("macism " .. last_insert_layout)
-    --         else
-    --             os.execute("macism " .. english_layout)
-    --         end
-    --     end,
-    -- })
-elseif is_linux then
-    local last_layout = "keyboard-us" -- English is default
-
-    local function get_fcitx_layout()
-        local f = io.popen("fcitx5-remote -n")
-        if f ~= nil then
-            local result = f:read("*all")
-            f:close()
-            if result then
-                return result:gsub("%s+", "")
-            end
-        end
-        return "keyboard-us" -- fallback English
-    end
-
-    local function set_fcitx_layout(layout)
-        os.execute("fcitx5-remote -s " .. layout)
-    end
-
-    vim.api.nvim_create_autocmd("InsertLeave", {
-        callback = function()
-            last_layout = get_fcitx_layout()
-            set_fcitx_layout("keyboard-us") -- change to English
-        end,
-    })
-
-    vim.api.nvim_create_autocmd("InsertEnter", {
-        callback = function()
-            set_fcitx_layout(last_layout)
-        end,
-    })
-
-    vim.api.nvim_create_autocmd("FocusGained", {
-        callback = function()
-            if vim.fn.mode() == "i" then
-                set_fcitx_layout(last_layout)
-            else
-                set_fcitx_layout("keyboard-us")
-            end
-        end,
-    })
+if not (is_mac or is_linux) then
+	return
 end
+
+local english = is_mac and "com.apple.keylayout.ABC" or "keyboard-us"
+local last_layout = english
+
+local function get_layout()
+	local cmd = is_mac and "macism" or "fcitx5-remote -n"
+	local f = io.popen(cmd)
+	if f then
+		local result = f:read("*all"):gsub("%s+", "")
+		f:close()
+		return result
+	end
+	return english
+end
+
+local function set_layout(layout)
+	local cmd = is_mac and ("macism " .. layout) or ("fcitx5-remote -s " .. layout)
+	os.execute(cmd)
+end
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+	callback = function()
+		last_layout = get_layout()
+		set_layout(english)
+	end,
+})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+	callback = function()
+		set_layout(last_layout)
+	end,
+})
+
+vim.api.nvim_create_autocmd("FocusGained", {
+	callback = function()
+		if vim.fn.mode() == "i" then
+			set_layout(last_layout)
+		else
+			set_layout(english)
+		end
+	end,
+})
