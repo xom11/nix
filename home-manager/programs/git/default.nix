cbca6110f90b20bb32d52ce251d6a5ffe564d3e2
khanhkhanhlele <namkhanh2172@gmail.com>
2026-03-12 17:05:35 +0700

    update

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -12,7 +12,8 @@ mkModule config ./. {
         email = "namkhanh20xx@gmail.com";
       };
       init.defaultBranch = "main";
-      credential.helper = "store";
+      credential.helper = "store"; # Cache credentials on disk (plain text, ~/.git-credentials)
+      push.autoSetupRemote = true; # Auto-set upstream on first push without needing -u origin <branch>
     };
   };
 }
