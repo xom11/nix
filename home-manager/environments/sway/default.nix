ca66f1cbf6406a8c9e064b7adf31a33463f66b2a
a <a>
2026-04-24 15:12:36 +0700

    lib: add mkApt helper for declarative apt package installation

--- home-manager/environments/sway/default.nix
+++ home-manager/environments/sway/default.nix
@@ -1,20 +1,13 @@
 {
   config,
   pkgs,
-  lib,
   getPath,
   mkModule,
+  mkApt,
   ...
 }:
 let
   pwd = getPath ./.;
-  # Packages that need system integration (GPU/DRM, PAM, logind)
-  # must be installed via apt instead of Nix on non-NixOS systems
-  aptPackages = [
-    "sway"
-    "swaylock"
-    "xdg-desktop-portal-wlr"
-  ];
 in
 mkModule config ./. {
   home.file = {
@@ -28,20 +21,7 @@ mkModule config ./. {
   xdg.configFile."environment.d/999-nix-path.conf".text = ''
     PATH=${config.home.homeDirectory}/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH
   '';
-  home.activation.aptPackages = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    if command -v apt-get &>/dev/null; then
-      missing=()
-      for pkg in ${lib.concatStringsSep " " aptPackages}; do
-        if ! dpkg -s "$pkg" &>/dev/null; then
-          missing+=("$pkg")
-        fi
-      done
-      if [ ''${#missing[@]} -gt 0 ]; then
-        echo "Installing apt packages: ''${missing[*]}"
-        sudo apt-get install -y "''${missing[@]}"
-      fi
-    fi
-  '';
+  home.activation = mkApt ./. ["sway" "swaylock" "xdg-desktop-portal-wlr"];
   home.packages = with pkgs; [
     libnotify
     mako
