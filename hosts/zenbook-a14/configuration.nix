1fcab483046664ab6f8460d985438226963baf0a
a <a>
2026-04-25 17:14:06 +0700

    update

--- hosts/zenbook-a14/configuration.nix
+++ hosts/zenbook-a14/configuration.nix
@@ -6,6 +6,10 @@
   imports = [
     ../../system-manager
   ];
-  modules.system-manager.services.kanata.enable = true;
-  modules.system-manager.services.tailscale.enable = true;
+  modules.system-manager = {
+    services = {
+      kanata.enable = true;
+      tailscale.enable = true;
+    };
+  };
 }
