319dc529c71f86c69d4c737c186fde65e93f2a1d
l3n4k4 <276671064+l3n4k4@users.noreply.github.com>
2026-04-26 13:27:47 +0700

    update

--- home-manager/programs/yazi/default.nix
+++ home-manager/programs/yazi/default.nix
@@ -9,12 +9,14 @@
 in
   mkModule config ./. {
     home.file = {
-      ".config/yazi" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${pwd}/yazi.d";
-      };
+      ".config/yazi/yazi.toml".source = config.lib.file.mkOutOfStoreSymlink "${pwd}/yazi.d/yazi.toml";
+      ".config/yazi/keymap.toml".source = config.lib.file.mkOutOfStoreSymlink "${pwd}/yazi.d/keymap.toml";
+      ".config/yazi/theme.toml".source = config.lib.file.mkOutOfStoreSymlink "${pwd}/yazi.d/theme.toml";
+      ".config/yazi/init.lua".source = config.lib.file.mkOutOfStoreSymlink "${pwd}/yazi.d/init.lua";
+      ".config/yazi/plugins/git.yazi".source = pkgs.yaziPlugins.git;
+      ".config/yazi/plugins/smart-enter.yazi".source = pkgs.yaziPlugins.smart-enter;
     };
     home.packages = [
       pkgs.yazi
-      pkgs.yaziPlugins.smart-enter
     ];
   }
