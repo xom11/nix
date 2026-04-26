1d0343bb70872e00ed86b79d28e4f85b09d49d2d
l3n4k4 <276671064+l3n4k4@users.noreply.github.com>
2026-04-26 13:34:33 +0700

    update

--- home-manager/programs/yazi/yazi.d/init.lua
+++ home-manager/programs/yazi/yazi.d/init.lua
@@ -1,12 +1,1 @@
--- function Linemode:size()
---   if self._file.cha.is_dir then
---     return ""
---   end
---   local size = self._file:size()
---   if not size then
---     return ""
---   end
---   return ui.Line(ya.readable_size(size))
--- end
-
 require("git"):setup()
