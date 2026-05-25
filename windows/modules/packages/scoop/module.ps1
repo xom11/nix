9b3de73b0b97287b98d3a2819d05e298a20754b8
l3n4k4 <l3n4k4@gmail.com>
2026-05-25 17:06:02 +0700

    fix(windows): add 'extras' scoop bucket (needed for kanata)

--- windows/modules/packages/scoop/module.ps1
+++ windows/modules/packages/scoop/module.ps1
@@ -2,7 +2,7 @@
     Description = 'Scoop: all portable CLI dev tools (scoop v0.5.3+ supports admin install)'
     Apply = {
         param($Ctx)
-        Install-ScoopPackages -Packages @(
+        Install-ScoopPackages -Buckets @('extras') -Packages @(
             'git'
             'nodejs'
             'gh'
