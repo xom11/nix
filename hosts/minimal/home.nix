8e09a75202694ced1e0a5a67653a1cda6458d051
l3n4k4 <l3n4k4@gmail.com>
2026-07-08 07:46:05 +0700

    update

--- hosts/minimal/home.nix
+++ hosts/minimal/home.nix
@@ -1,12 +1,10 @@
-{ pkgs, device, ... }:
+{ pkgs, ... }:
 let
-  cfgDir = "~/.nix/hosts/${device}";
 in
 {
   imports = [
     ../../home-manager
   ];
-  # Override homeDirectory to match VM HOME (/home/lenamkhanh.guest instead of /home/lenamkhanh)
   home.homeDirectory = builtins.getEnv "HOME";
   home.sessionVariables = {
       LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
