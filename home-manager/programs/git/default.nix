33f089b06c36d5b95a133415f34175880fc0f125
l3n4k4 <276671064+l3n4k4@users.noreply.github.com>
2026-07-16 17:05:50 +0700

    update

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -13,9 +13,9 @@
 in
   mkModule config ./. {
     home.file = {
-      # ".config/git/config" = {
-      #   source = config.lib.file.mkOutOfStoreSymlink "${pwd}/git.d/config";
-      # };
+      ".config/git/config" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${pwd}/git.d/config";
+      };
       ".config/gh-dash/config.yml" = {
         source = config.lib.file.mkOutOfStoreSymlink "${pwd}/gh-dash.d/config.yml";
       };
